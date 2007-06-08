98cb278e6b393a2269e0ffb07d420fb6694bf257
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-08 20:43:54 +0000

    Adding authorize function to base services.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@195 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_service.php
+++ src/cognifty/lib/lib_cgn_service.php
@@ -3,11 +3,30 @@
 class Cgn_Service {
 
 	var $presenter = 'default';
+	var $requireLogin = false;
 
 	function processEvent($e,&$req,&$t) {
 		$eventName = $e.'Event';
 		$this->$eventName($req,$t);
 	}
 
+	/**
+	 * Signal whether or not the user can access
+	 * this service given event $e
+	 */
+	function authorize($e, $u) {
+		if ($this->requireLogin && $u->isAnonymous() ) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Signal whether or not the user can perform the
+	 *  specified action on the specified data item.
+	 */
+	function authorizeAction($e, $a, $d, $u) {
+		return true;
+	}
 }
 ?>
