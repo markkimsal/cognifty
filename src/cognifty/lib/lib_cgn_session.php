6cfe9e02b91ac0a490f600ee0bda6ffe5060d5b3
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-08 23:55:05 +0000

    Adding commit and re-auth functions.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@201 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_session.php
+++ src/cognifty/lib/lib_cgn_session.php
@@ -13,53 +13,90 @@ class Cgn_Session {
 
 	var $sessionId = '';
 	var $started = FALSE;
+	var $sessionName = 'CGNSESSION';
+	var $timeout = 3600; //one hour timeout
+	var $authTime = -1;
+	var $touchTime = -1;
 
-	function Cgn_Session() {
-
-	}
+	function Cgn_Session() { }
 
 	function start() {
-
+       		$this->started = TRUE;
+		session_start();
+		$this->sessionId = session_id();
+		$this->touch();
 	}
 
-	function close() { 
+	function close() { }
 
-	}
+	function set($key, $val) { }
 
-	function set($key, $val) {
+	function setArray($a) { }
 
-	}
-
-	function get($key) { 
-
-	}
+	function get($key) { }
 	
-	function getSessionId() { 
+	function getSessionId() { }
 
+	function isSessionStale() { }
+
+	/**
+	 * Set a usage time stamp for this session.
+	 */
+	function touch($t=0) {
+		if ($t == 0) {
+			$this->touchTime = time();
+		} else {
+			$this->touchTime = $t;
+		}
 	}
 
-	function isSessionStale() {
-
+	/**
+	 * Set the last time a session was authorized, as in
+	 * a user submitting a password
+	 */
+	function setAuthTime($t=0) { 
+		if ($t == 0) {
+			$this->authTime = time();
+		} else {
+			$this->authTime = $t;
+		}
 	}
 
-	function needsReAuth() {
+	/**
+	 * Return true if the activity has been to long and
+	 * the system wants re-authorization.  The session is still
+	 * active, but this function recommends asking for a new
+	 * password for more security.
+	 */
+	function needsReAuth() { 
+		if ( (time() - $this->get('_touch')) >= $this->timeout ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+	/**
+	 * Flush needed variables to the session storage.
+	 * For DB based sessions these values might need to go
+	 * into the table and not the session itself.
+	 */
+	function commit() { 
+		$this->set('_auth', $this->authTime);
+		$this->set('_touch', $this->touchTime);
 	}
 }
 
 
-class Cgn_Session_Simple {
+class Cgn_Session_Simple extends Cgn_Session {
 
 
 	function Cgn_Session_Simple() { 
-		session_name('CGNSESSION');
+		session_name($this->sessionName);
 		$this->start();
 	}
 
 	function start() {
-		$this->started = TRUE;
-		session_start();
-		$this->sessionId = session_id();
 	}
 
 	function close() { 
@@ -72,6 +109,12 @@ class Cgn_Session_Simple {
 
 	function get($key) { 
 		return $_SESSION[$key];
+	}
+
+	function setArray($a) {
+		foreach ($a as $key=>$val) {
+			$_SESSION[$key] = $val;
+		}
 	}
 }
 ?>
