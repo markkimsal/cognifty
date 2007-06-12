6171833b553fb58b2c7917f46036aea4efd0c147
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-12 17:22:56 +0000

    Very simple way to publish content to the publish table.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@241 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/edit.php
+++ src/cognifty/admin/content/edit.php
@@ -20,9 +20,45 @@ class Cgn_Service_Content_Edit extends Cgn_Service_Admin {
 
 		//cut up the data into table data
 		while ($db->nextRecord()) {
-			$t['data'][] = 	$db->record;
+			$t['data'] = $db->record;
 		}
+		$t['form'] = $this->_loadPublishForm(
+			array('id'=>$t['data']['cgn_content_id']));
+	}
+
+
+	function publishEvent(&$req, &$t) {
+		$id = $req->cleanInt('id');
+		$cont = new Cgn_DataItem('cgn_content');
+		$cont->_pkey = 'cgn_content_id';
+		$cont->load($id);
+		//save to publish table
+		$cont->_table = 'cgn_content_publish';
+		$cont->_pkey = 'cgn_content_publish_id';
+		unset($cont->cgn_content_id);
+		$newId = $cont->save();
+		//update main table with the id of the published content
+		/* finish this later
+		$pubId = $cont->cgn_content_publish_id;
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query("UPDATE cgn_content SET cgn_content_publish_id = ".$pubId." WHERE cgn_content_id = ".$id);
+		 */
+
+		$this->presenter = 'redirect';
+		$t['url'] = cgn_adminurl(
+			'content','main');
+	}
+
+
+	function _loadPublishForm($values=array()) {
+		include_once('../cognifty/lib/form/lib_cgn_form.php');
+		include_once('../cognifty/lib/html_widgets/lib_cgn_widget.php');
+		$f = new Cgn_Form('publish');
+		$f->action = cgn_adminurl('content','edit','publish');
+		$f->label = 'Publish Content';
+		$f->appendElement(new Cgn_Form_ElementHidden('id'),$values['id']);
+		$f->appendElement(new Cgn_Form_ElementHidden('event'),'save');
+		return $f;
 	}
 }
-
 ?>
---
+++ src/cognifty/admin/content/templates/edit_main.html.php
@@ -0,0 +1,4 @@
+<h3><?=$t['data']['title'];?></h3>
+<span style="font-size:75%;"><?=$t['data']['caption'];?></span>
+
+<? echo $t['form']->toHtml(); ?>
