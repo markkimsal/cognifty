be3f1c4a23e8f1691edee90d4ab7a5f94ede9cd6
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-12 01:31:10 +0000

    Remove debug_backtrace call.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@220 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_db_mysql.php
+++ src/cognifty/lib/lib_cgn_db_mysql.php
@@ -71,8 +71,6 @@ include_once(CGN_LIB_PATH."/lib_cgn_db_master.php");
 					trigger_error('database error: ('.$this->errorNumber.') '.$this->errorMessage.'
 						<br/> statement was: <br/>
 						'.$queryString);
-					print_r(
-					debug_backtrace());
 				}
 			}
 			if (is_resource($resSet) )
