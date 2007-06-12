8cd0c61b8ba69f3f338cd1cf52b288e60af4160d
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-12 18:39:40 +0000

    Switching content publish for article publish.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@248 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- setup/db_install/cgn_article_publish.mysql.sql
+++ setup/db_install/cgn_article_publish.mysql.sql
@@ -4,10 +4,12 @@
 -- generated on: 06.12.2007
 
 
-DROP TABLE IF EXISTS `cgn_content_publish`;
-CREATE TABLE `cgn_content_publish` (
+DROP TABLE IF EXISTS `cgn_article_publish`;
+CREATE TABLE `cgn_article_publish` (
 		
-	`cgn_content_publish_id` integer (11) NOT NULL auto_increment, 
+	`cgn_article_publish_id` integer (11) NOT NULL auto_increment, 
+	`cgn_content_id` integer (11) NOT NULL, 
+	`cgn_content_version` integer (11) NOT NULL, 
 	`cgn_guid` varchar (255) NOT NULL, 
 	`title` varchar (255) NOT NULL, 
 	`type` varchar (255) NOT NULL, 
@@ -15,10 +17,7 @@ CREATE TABLE `cgn_content_publish` (
 	`mime` varchar (255) NOT NULL, 
 	`caption` varchar (255) NOT NULL, 
 	`description` text NOT NULL, 
-	`notes` text NOT NULL, 
 	`content` text NOT NULL, 
-	`binary` text NOT NULL, 
-	`filename` varchar (255) NOT NULL, 
 	`link_text` varchar (255) NOT NULL,
-	PRIMARY KEY (cgn_content_publish_id) 
+	PRIMARY KEY (cgn_article_publish_id) 
 );
