40447b52c5a252ced5ca72796a5006727e705913
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-15 19:05:23 +0000

    show the groups a user belongs to on the user view page.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@308 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/users/templates/view_main.html.php
+++ src/cognifty/admin/users/templates/view_main.html.php
@@ -2,3 +2,20 @@
 Email:  <?= $t['users']->email;?>
 <br/>
 User ID:  <?= $t['users']->cgn_user_id;?>
+
+<br/>
+<br/>
+Belongs to group(s):
+<?php
+if ( is_array($t['groups']) ) {
+?>
+<ul>
+	<?php
+	foreach ($t['groups'] as $grp) {
+		echo "<li>".$grp['display_name']."</li>\n";
+	}
+?>
+</ul>
+<?php
+}
+?>
--- src/cognifty/admin/users/view.php
+++ src/cognifty/admin/users/view.php
@@ -14,6 +14,16 @@ class Cgn_Service_Users_View extends Cgn_Service_Admin {
 		$id = $req->cleanInt('id');
 		$t['users'] = new Cgn_DataItem('cgn_user');
 		$t['users']->load($id);
+
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query("SELECT A.display_name , A.cgn_group_id
+			FROM cgn_group as A
+			LEFT JOIN cgn_user_group_link ON A.cgn_group_id = cgn_user_group_link.cgn_group_id
+			WHERE cgn_user_group_link.cgn_user_id = $id");
+
+		while ($db->nextRecord()) {
+			$t['groups'][] = $db->record;
+		}
 	}
 }
 ?>
