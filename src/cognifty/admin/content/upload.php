0718d0bf50eb15efb0a9808479c7f7a2fea64c86
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-15 16:13:32 +0000

    Added Files option to the Admin Menu.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@286 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/upload.php
+++ src/cognifty/admin/content/upload.php
@@ -14,7 +14,6 @@ class Cgn_Service_Content_Upload extends Cgn_Service_Admin {
 		$t['form'] = $this->_loadContentForm();
 	}
 
-
 	function saveUploadEvent(&$req, &$t) {
 		$content = new Cgn_DataItem('cgn_content');
 //		print_r($_FILES);exit();
@@ -32,26 +31,6 @@ class Cgn_Service_Content_Upload extends Cgn_Service_Admin {
 		$t['url'] = cgn_adminurl(
 			'content','main');
 	}
-
-
-
-	function _loadPublishForm($values=array()) {
-		include_once('../cognifty/lib/form/lib_cgn_form.php');
-		include_once('../cognifty/lib/html_widgets/lib_cgn_widget.php');
-		$f = new Cgn_Form('publish');
-		$f->action = cgn_adminurl('content','edit','publishAs');
-		$f->label = 'Publish Content';
-		$radio = new Cgn_Form_ElementRadio('subtype','Choose a type');
-		$radio->addChoice('Article');
-		$radio->addChoice('Blog');
-		$radio->addChoice('News');
-		$f->appendElement($radio);
-		$f->appendElement(new Cgn_Form_ElementHidden('id'),$values['id']);
-		$f->appendElement(new Cgn_Form_ElementHidden('event'),'publishAs');
-		return $f;
-	}
-
-
 
 	function _loadContentForm($values=array()) {
 		include_once('../cognifty/lib/form/lib_cgn_form.php');
