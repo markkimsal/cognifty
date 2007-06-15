29cbdf755eb572e91eff55063a71e9817187c91e
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-15 01:39:20 +0000

    content binary field needs to be blob to hold binary data.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@284 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- setup/db_install/cgn_content.mysql.sql
+++ setup/db_install/cgn_content.mysql.sql
@@ -1,7 +1,7 @@
 -- Dumping SQL for project cognifty
 -- entity version: 0.0
 -- DB type: mysql
--- generated on: 06.12.2007
+-- generated on: 06.14.2007
 
 
 DROP TABLE IF EXISTS `cgn_content`;
@@ -16,8 +16,8 @@ CREATE TABLE `cgn_content` (
 	`caption` varchar (255) NOT NULL, 
 	`description` text NOT NULL, 
 	`notes` text NOT NULL, 
-	`content` text NOT NULL, 
-	`binary` text NOT NULL, 
+	`content` text, 
+	`binary` longblob, 
 	`filename` varchar (255) NOT NULL, 
 	`link_text` varchar (255) NOT NULL,
 	PRIMARY KEY (cgn_content_id) 
