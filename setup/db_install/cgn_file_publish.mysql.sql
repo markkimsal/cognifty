0ca06dc013990e6f01dc125a617940eeac8f9e5a
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-15 16:46:21 +0000

    Adding file publish Mysql table structure.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@294 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ setup/db_install/cgn_file_publish.mysql.sql
@@ -0,0 +1,22 @@
+-- Dumping SQL for project cognifty
+-- entity version: 0.0
+-- DB type: mysql
+-- generated on: 06.15.2007
+
+DROP TABLE IF EXISTS `cgn_file_publish`;
+
+CREATE TABLE `cgn_file_publish` (
+  `cgn_file_publish_id` int(11) NOT NULL auto_increment,
+  `cgn_content_id` int(11) NOT NULL,
+  `cgn_content_version` int(11) NOT NULL,
+  `cgn_guid` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `sub_type` varchar(255) NOT NULL,
+  `mime` varchar(255) NOT NULL,
+  `caption` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `content` longblob NOT NULL,
+  `link_text` varchar(255) NOT NULL,
+  PRIMARY KEY  (`cgn_file_publish_id`)
+);
