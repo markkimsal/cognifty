f9bad61964444d8090a17a682b0f3b40e78054de
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-15 18:24:24 +0000

    Started some basic View Articles code
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@304 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/articles.php
+++ src/cognifty/admin/content/articles.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('../cognifty/lib/html_widgets/lib_cgn_widget.php');
+include_once('../cognifty/lib/lib_cgn_mvc.php');
 
 class Cgn_Service_Content_Articles extends Cgn_Service_Admin {
 
@@ -7,8 +9,46 @@ class Cgn_Service_Content_Articles extends Cgn_Service_Admin {
 	}
 
 	function mainEvent(&$sys, &$t) {
-		Cgn_Template::assignString('Message1','This is the main event!');
+//		Cgn_Template::assignString('Message1','This is the main event!');
+	
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query('select * from cgn_article_publish');
+
+		$list = new Cgn_Mvc_TableModel();
+
+		//cut up the data into table data
+		while ($db->nextRecord()) {
+			$list->data[] = array(
+				$db->record['title'],
+				$db->record['caption'],
+				$db->record['type'],
+				$db->record['sub_type'],
+				cgn_adminlink('view','content','view','',array('id'=>$db->record['cgn_content_id'])),
+				cgn_adminlink('edit','content','edit','',array('id'=>$db->record['cgn_content_id']))
+			);
+		}
+		$list->headers = array('Title','Sub-Title','Type','Used as','View','Edit');
+//		$list->columns = array('title','caption','content');
+
+		/*
+		$list->data = array(
+			0=> array('link 1','foobar.php'),
+			1=> array('link 2','foobar.php'),
+			2=> array('link 3','foobar.php')
+		);
+		 */
+
+//		$t['listPanel'] = new Cgn_ListView($list);
+//		Cgn_Template::assignObject('listPanel',$t['listPanel']);
+
+//		$t['menuPanel'] = new Cgn_Menu('Sample Menu',$list);
+		$t['menuPanel'] = new Cgn_Mvc_TableView($list);
+		$t['menuPanel']->style['width'] = 'auto';
+		$t['menuPanel']->style['border'] = '1px solid black';
+
+		$t['link'] = '<a href="'.cgn_adminurl('content','main','addArticle').'">Add</a>';
 	}
 }
+
 
 ?>
