eb8d8c4501eb73c19dcace054472325d14a81175
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-16 20:41:13 +0000

    Add some javascript to show how linking to other documents would work.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@334 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/templates/edit_main.html.php
+++ src/cognifty/admin/content/templates/edit_main.html.php
@@ -3,3 +3,11 @@ echo $t['form']->toHtml();
 ?>
 
 Version: <?= $t['version'];?>
+
+<br/>
+<fieldset>
+<legend>Insert Other Content</legend>
+<a onclick="insertTags('{{img:','}}','Image Title');return false" href="#">Web image</a>
+<br/>
+<a onclick="insertTags('[[',']]','Article Title');return false" href="#">link to article</a>
+</fieldset>
