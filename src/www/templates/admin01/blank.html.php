a5bb5263032ee8b416264e07c245522df9d25562
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-17 20:23:29 +0000

    This blank template is great for pop-ups and iframes.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@350 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/www/templates/admin01/blank.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title><?php cgn_sitename();?> Control Center</title>
+    <link href="<?php cgn_templateurl();?>admin01-screen.css" rel="stylesheet" type="text/css" />
+    <script language="JavaScript" src="<?=cgn_templateurl();?>menu.js" type="text/javascript"></script>
+    <script language="JavaScript" src="<?=cgn_templateurl();?>wiki.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+		<div id="contentcontent">
+			<?php Cgn_Template::parseTemplateSection('content.main'); ?>
+		</div>
+</body>
+</html>
