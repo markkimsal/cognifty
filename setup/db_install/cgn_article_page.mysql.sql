68f719c5ac97508901155a401c67f1337300d625
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-17 01:40:46 +0000

    Adding table to hold multiple pages.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@336 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ setup/db_install/cgn_article_page.mysql.sql
@@ -0,0 +1,17 @@
+-- Dumping SQL for project cognifty
+-- entity version: 0.0
+-- DB type: mysql
+-- generated on: 06.12.2007
+
+
+DROP TABLE IF EXISTS `cgn_article_page`;
+CREATE TABLE `cgn_article_page` (
+		
+	`cgn_article_page_id` integer (11) NOT NULL auto_increment, 
+	`cgn_article_publish_id` integer (11) NOT NULL default 0, 
+	`title` varchar (255) NOT NULL, 
+	`content` text NOT NULL, 
+	PRIMARY KEY (cgn_article_page_id) 
+);
+
+CREATE INDEX cgn_article_publish_idx ON `cgn_article_page` (`cgn_article_publish_id`);
