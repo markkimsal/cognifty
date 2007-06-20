51d969497117b6ecdf40287ce6539ed67d20cefd
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-20 03:02:18 +0000

    cleanup interface for article admin.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@370 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/articles.php
+++ src/cognifty/admin/content/articles.php
@@ -22,33 +22,17 @@ class Cgn_Service_Content_Articles extends Cgn_Service_Admin {
 			$list->data[] = array(
 				$db->record['title'],
 				$db->record['caption'],
-				$db->record['type'],
-				$db->record['sub_type'],
 				cgn_adminlink('view','content','view','',array('id'=>$db->record['cgn_content_id'])),
 				cgn_adminlink('edit','content','edit','',array('id'=>$db->record['cgn_content_id'])),
 				cgn_adminlink('delete','content','delete','',array('id'=>$db->record['cgn_content_id']))
 			);
 		}
-		$list->headers = array('Title','Sub-Title','Type','Used as','View','Edit','Delete');
-//		$list->columns = array('title','caption','content');
+		$list->headers = array('Title','Sub-Title','View','Edit','Delete');
 
-		/*
-		$list->data = array(
-			0=> array('link 1','foobar.php'),
-			1=> array('link 2','foobar.php'),
-			2=> array('link 3','foobar.php')
-		);
-		 */
-
-//		$t['listPanel'] = new Cgn_ListView($list);
-//		Cgn_Template::assignObject('listPanel',$t['listPanel']);
-
-//		$t['menuPanel'] = new Cgn_Menu('Sample Menu',$list);
 		$t['menuPanel'] = new Cgn_Mvc_TableView($list);
 		$t['menuPanel']->style['width'] = 'auto';
 		$t['menuPanel']->style['border'] = '1px solid black';
 
-		$t['link'] = '<a href="'.cgn_adminurl('content','main','addArticle').'">Add</a>';
 	}
 }
 
