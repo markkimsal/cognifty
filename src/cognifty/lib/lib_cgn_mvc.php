54eba0f82df2e1aa1e74b8eddbf75bf67a32e6f3
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-22 21:20:46 +0000

    Moving admin data grid specialization into standard library.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@425 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_mvc.php
+++ src/cognifty/lib/lib_cgn_mvc.php
@@ -192,53 +192,6 @@ class Cgn_Mvc_ListModel extends Cgn_Mvc_DefaultItemModel {
 }
 
 
-class Cgn_Mvc_TableModel extends Cgn_Mvc_DefaultItemModel {
-	var $data = array();
-	var $columns = array();
-	var $headers = array();
-
-	function Cgn_Mvc_TableModel() {
-		$x = new Cgn_Mvc_ModelNode();
-		$this->setRootNode($x); 
-//		$this->addColumn();
-	}
-
-	function getHeader($modelNode, $dataRole = null) { 
-//		if (count($this->headers) < 1) { return null; }
-
-		if (is_null($modelNode->col)) {
-			return $this->headers[$modelNode->row];
-		} else {
-			return $this->headers[$modelNode->col];
-		}
-	}
-
-	function getValue($modelNode, $dataRole = null) { 
-		if (is_null($modelNode->col)) {
-			return $this->data[$modelNode->row];
-		} else {
-			if (count($this->columns) > 0) {
-				$colName = $this->columns[$modelNode->col];
-				return $this->data[$modelNode->row][$colName];
-			} else {
-				return $this->data[$modelNode->row][$modelNode->col];
-			}
-		}
-	}
-
-	function getRowCount() { 
-		return count($this->data);
-	}
-
-	function getColumnCount() { 
-//		if (count($this->columns) ) {
-//			return count($this->columns);
-//		}
-		return intval(@count($this->data[0]));
-	}
-}
-
-
 class Cgn_Mvc_AbstractItemView  extends Cgn_HtmlWidget {
 	var $altRowColors = true;
 	var $iconSize = 32;
@@ -276,60 +229,5 @@ class Cgn_Mvc_ListView extends Cgn_Mvc_AbstractItemView {
 		return $html;
 	}
 }
-
-
-
-class Cgn_Mvc_TableView extends Cgn_Mvc_AbstractItemView {
-
-	var $tagName = 'table';
-	var $type    = 'table';
-	var $classes = array('grid_1');
-	var $attribs = array('border'=>2);
-
-	function Cgn_Mvc_TableView(&$model) {
-		$this->setModel($model);
-	}
-
-
-	function setModel(&$m) {
-		//fire data changed event
-		$this->_model =& $m;
-	}
-
-
-	function toHtml($id='') {
-		$html  = '';
-		$html .= $this->printOpen();
-		$rows = $this->_model->getRowCount();
-		$cols = $this->_model->getColumnCount();
-
-		//do table headers
-		$headers = $this->_model->headers;
-		if (count($headers) > 0) { 
-			$html .= '<tr class="grid_tr_h">'."\n";
-			for($y=0; $y < $cols; $y++) {
-				$datum = $this->_model->getHeaderAt(null,$y);
-				$html .= '<th class="grid_th_1">'.$datum.'</th>'."\n";
-			}
-			$html .= '</tr>'."\n";
-		}
-
-		for($x=0; $x < $rows; $x++) {
-			$html .= '<tr class="grid_tr_1">'."\n";
-			for($y=0; $y < $cols; $y++) {
-				if ($x%2==0) {$class = 'grid_td_1';} else {$class = 'grid_td_2';}
-				$datum = $this->_model->getValueAt($x,$y);
-				$html .= '<td class="'.$class.'">'.$datum.'</td>'."\n";
-			}
-			$html .= '</tr>'."\n";
-		}
-		if ($rows < 1) {
-			$html .= '<tr class="grid_tr_1"><td><em>No records found.</em></td></tr>';
-		}
-		$html .= $this->printClose();
-		return $html;
-	}
-}
-
 
 ?>
