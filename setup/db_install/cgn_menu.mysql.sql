36f63ee3a8a36999d904a73659c5bc48bd5cf60c
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-06-22 19:46:39 +0000

    Adding menu table.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@418 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ setup/db_install/cgn_menu.mysql.sql
@@ -0,0 +1,20 @@
+-- Dumping SQL for project cognifty
+-- entity version: 0.0
+-- DB type: mysql
+-- generated on: 06.14.2007
+
+
+DROP TABLE IF EXISTS `cgn_menu`;
+CREATE TABLE `cgn_menu` (
+		
+	`cgn_menu_id` integer (11) NOT NULL auto_increment, 
+	`title` varchar (255) NOT NULL, 
+	`code_name` varchar (255) NOT NULL, 
+	`edited_on` integer (11) NOT NULL default 1,
+	`created_on` integer (11) NOT NULL default 1,
+	PRIMARY KEY (cgn_menu_id) 
+);
+
+CREATE INDEX code_name_idx ON cgn_menu (`code_name`);
+CREATE INDEX edited_on_idx ON cgn_menu (`edited_on`);
+CREATE INDEX created_on_idx ON cgn_menu (`edited_on`);
