c8a2fe5cc991919dba8828aced249c1157131580
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-07-13 22:48:58 +0000

    simple message change for tests.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@483 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- tests/error_test.php
+++ tests/error_test.php
@@ -8,17 +8,15 @@ class TestOfErrors extends UnitTestCase {
 
 	function testError() {
 
-		trigger_error('php user error');
+		trigger_error('from trigger error');
 		$e2 = Cgn_ErrorStack::pullError();
 
 		Cgn_ErrorStack::throwError('my error');
 		$e1 = Cgn_ErrorStack::pullError();
 
-//		echo "<pre>";
-//		print_r($e2);
-
 		$this->assertEqual('cgn_runtimeerror', strtolower( get_class( $e1 ) ));
 		$this->assertEqual('cgn_runtimeerror', strtolower( get_class( $e2 ) ));
+		$this->assertEqual( $e2->message, 'from trigger error' );
 	}
 }
 ?>
