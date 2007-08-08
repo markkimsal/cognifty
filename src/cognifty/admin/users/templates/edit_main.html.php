40075c9e6c940a6943d0bb0ab737eb3be2378ac1
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-08-08 04:05:26 +0000

    Added some basic functionality to edit and delete a user
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@566 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/admin/users/templates/edit_main.html.php
@@ -0,0 +1,14 @@
+<fieldset><legend>Edit a User</legend>
+<br />
+<h4> Edit the User's information below  OR  
+&nbsp;<a href="<?=cgn_adminurl('users','main');?>" >Cancel This</a> : </h4>
+<br />
+<br />
+<?php
+	echo $t['form01']->toHtml();
+?>
+<br/>
+<h4>  NOTE : *  Indicates a required field. </h4>
+<h4>  NOTE : If you leave the passwords blank, they WILL NOT be overwritten.
+</fieldset>
+<br/>
