0d320750df4a4fd15294f28f2afab330c42be187
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-08-18 14:11:31 +0000

    tweaked user view details
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@607 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/users/templates/view_main.html.php
+++ src/cognifty/admin/users/templates/view_main.html.php
@@ -1,21 +1,46 @@
-<h3>Username: <?= $t['users']->username;?></h3>
-Email:  <?= $t['users']->email;?>
-<br/>
-User ID:  <?= $t['users']->cgn_user_id;?>
+<h2>View Users</h2>
+<br />
 
-<br/>
-<br/>
-Belongs to group(s):
-<?php
-if ( is_array($t['groups']) ) {
-?>
-<ul>
+<table  width="auto" cellpadding="3" cellspacing="3" border="2" bgcolor="#EEEEEE">
+<tr><td>
+
+	<table align="left" cellpadding="0" cellspacing="0" style="border: 1px solid #C0C0C0; background-color: #EEEEEE ;" >
+	<tr><td nowrap>
+
 	<?php
-	foreach ($t['groups'] as $grp) {
-		echo "<li>".$grp['display_name']."</li>\n";
-	}
-?>
-</ul>
-<?php
-}
-?>
+		echo '<table  width="600px" border="0px" cellpadding="3" cellspacing="3">'."\n";
+		echo '<tr>'."\n";
+		echo '<td colspan="2">&nbsp;</td>'."\n";
+		echo '</tr><tr>'."\n";
+		echo '<td align="right">Record :</td><td align="left">'.$t['users']->cgn_user_id.'</td>'."\n";
+		echo '</tr><tr>'."\n";
+		echo '<td width="200px" align="right" nowrap>Username :</td><td align=\"left\"><h3>'.$t['users']->username.'</h3></td>'."\n";
+		echo '</tr><tr>'."\n";
+		echo '<td align="right">Email :</td><td align="left"><h3>'.$t['users']->email.'</h3></td>'."\n";
+		echo '</tr><tr>'."\n";
+		echo '<td align="right">Belongs to group(s):</td>'."\n";
+		echo '<td align="left"><ul style="font-weight:bold;list-style:none;">';
+			if (is_array($t['groups'])) {
+				foreach ($t['groups'] as $grp) {
+					echo "<li>".$grp['display_name']."</li>\n";
+				}
+			} else {
+					echo "<li>NONE</li>\n";
+			}
+		echo '</ul></td>'."\n";
+		echo '</tr><tr>'."\n";
+		echo '<td align="right" colspan="2">';
+		echo '<form >'."\n";
+		echo '<input style="width:7em;" class="formbutton" type="button" name="viewuser01_cancel" onclick="javascript:history.go(-1);" value="Return"/>';
+		echo "\n";
+		echo '</form >'."\n";
+		echo '</td>'."\n";
+		echo '</tr>'."\n";
+		echo '</table>'."\n";
+	?>
+
+	</td></tr>
+	</table>
+
+</td></tr>
+</table>
