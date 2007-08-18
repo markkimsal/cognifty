5314d2ce381eed03036b4e8e8d86f2702c9dfe03
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-08-18 14:28:26 +0000

    tweaked user view details again
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@608 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/users/templates/view_main.html.php
+++ src/cognifty/admin/users/templates/view_main.html.php
@@ -17,18 +17,19 @@
 		echo '<td width="200px" align="right" nowrap>Username :</td><td align=\"left\"><h3>'.$t['users']->username.'</h3></td>'."\n";
 		echo '</tr><tr>'."\n";
 		echo '<td align="right">Email :</td><td align="left"><h3>'.$t['users']->email.'</h3></td>'."\n";
-		echo '</tr><tr>'."\n";
-		echo '<td align="right">Belongs to group(s):</td>'."\n";
-		echo '<td align="left"><ul style="font-weight:bold;list-style:none;">';
-			if (is_array($t['groups'])) {
-				foreach ($t['groups'] as $grp) {
-					echo "<li>".$grp['display_name']."</li>\n";
-				}
-			} else {
-					echo "<li>NONE</li>\n";
+		echo '</tr>'."\n";
+		if (is_array($t['groups'])) {
+			echo '<tr><td align="right">Belongs to group(s):</td><td align="left" style="font-weight:bold;">'."\n";
+			foreach ($t['groups'] as $grp) {
+				echo $grp['display_name']."</td>\n";
+				echo '</tr><tr><td align="right">&nbsp;</td><td align="left" style="font-weight:bold;">'."\n";
 			}
-		echo '</ul></td>'."\n";
-		echo '</tr><tr>'."\n";
+		} else {
+			echo '<tr><td align="right">Belongs to group(s):</td>'."\n";
+			echo '<td align="left" style="font-weight:bold;">NONE</td>'."\n";
+			echo '</tr>'."\n";
+		}
+		echo '<tr>'."\n";
 		echo '<td align="right" colspan="2">';
 		echo '<form >'."\n";
 		echo '<input style="width:7em;" class="formbutton" type="button" name="viewuser01_cancel" onclick="javascript:history.go(-1);" value="Return"/>';
