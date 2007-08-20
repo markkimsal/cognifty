72b056a896cab6e80e54515e79e6c8e32583d0ec
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-08-20 00:57:20 +0000

    Adding queue management page.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@620 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/admin/mxq/main.php
@@ -0,0 +1,42 @@
+<?php
+include_once('../cognifty/lib/html_widgets/lib_cgn_widget.php');
+include_once('../cognifty/lib/html_widgets/lib_cgn_toolbar.php');
+include_once('../cognifty/lib/lib_cgn_mvc.php');
+include_once('../cognifty/lib/lib_cgn_mvc_table.php');
+
+
+
+class Cgn_Service_Mxq_Main extends Cgn_Service_Admin {
+
+	function Cgn_Service_Mxq_Main () {
+
+	}
+
+
+	function mainEvent(&$sys, &$t) {
+		$db = Cgn_Db_Connector::getHandle();
+
+		$channelLoader = new Cgn_DataItem('cgn_mxq_channel');
+		$channels = $channelLoader->find();
+
+		$db->query(' SELECT count(cgn_mxq_id) as total, cgn_mxq_channel_id
+			FROM cgn_mxq
+			GROUP BY cgn_mxq_channel_id');
+
+		while ($db->nextRecord()) {
+			$cid = $db->record['cgn_mxq_channel_id'];
+			$channels[$cid]->total = $db->record['total'];
+		}
+
+		$model = new Cgn_Mvc_TableModel();
+
+		$model->headers = array('Channel Name','Message Count');
+		foreach ($channels as $chan) {
+			$model->data[] = array($chan->name, $chan->total);
+		}
+
+		$t['dataGrid'] = new Cgn_Mvc_TableView($model);
+	}
+}
+
+?>
