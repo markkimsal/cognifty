55450ecd9b3ffe740dfe97497f572e9b19a612ca
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-08-22 19:28:35 +0000

    Change icon for web pages to html mimetype.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@645 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/preview.php
+++ src/cognifty/admin/content/preview.php
@@ -80,7 +80,7 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 		while ($db->nextRecord()) {
 
 			$str = '<div onclick="parent.insertPage(\''.$db->record['link_text'].'\',\''.$db->record['title'].'\',\''.$db->record['cgn_content_id'].'\');" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
-			$str .= '<img src="'.cgn_url().'icons/default/document.png" align="left"/>';
+			$str .= '<img src="'.cgn_url().'icons/default/html.png" align="left"/>';
 			$str .= $db->record['title'].'</div>';
 			$t['data'][] = $str;
 		}
