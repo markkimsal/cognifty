45c3d1cae1c322fe497caa349a5bb345e51c6fe0
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-09-09 15:27:40 +0000

    Changed format. Updated format did not handle long titles well. Moved page links with titles to bottom of page. Disabled Page Number Only links. Slight change to formatting of page.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@671 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/main/templates/content_main.html.php
+++ src/cognifty/modules/main/templates/content_main.html.php
@@ -1,8 +1,19 @@
 
+	<h3><?= $t['title'];?></h3>
+	<?php if($t['caption'] != '') {  ?>
+	<span style="padding-left:1em;font-weight:bold;"><?= $t['caption'];?></span> 
+	<?php }  ?>
+
+	<div name="upper" filter="debug/debugHtml text/uc" class="content_wrapper">
+	<?php echo $t['content'];?>
+	</div>   
+
 	<?php
 		if ($t['hasPages']) {
-			echo "<div style=\"float:right;border:1px solid grey;margin-top:1em;padding:2px;\">\n";
-			echo "Page 1:&nbsp; ";
+			// echo "<div style=\"float:left;border:1px solid grey;margin-top:1em;padding:2px;\">\n";
+			echo "<hr>";
+			echo "<div style=\"float:left;\">\n";
+			echo "Page : 1 - ";
 				echo '<a href="';
 				echo cgn_appurl(
 					'main','
@@ -10,11 +21,15 @@
 					''
 				);
 				echo $t['article']->link_text;
-				echo '">'.$t['article']->title.'</a>';
+				echo '" ';
+				echo 'style=" line-height: 2em;"';
+				echo '>';
+				echo $t['article']->title;
+				echo '</a>';
 				echo "<br/> ";
 
 			foreach ($t['nextPages'] as $idx=>$pageTitle) {
-				echo "Page ".($idx+2).":&nbsp; ";
+				echo "Page : ".($idx+2)." - ";
 				echo '<a href="';
 				echo cgn_appurl(
 					'main','
@@ -22,26 +37,20 @@
 					''
 				);
 				echo $t['article']->link_text.'/p='.($idx+2);
-				echo '">'.$pageTitle.'</a>';
+				echo '" ';
+				echo 'style=" line-height: 2em;"';
+				echo '>';
+				echo $pageTitle;
+				echo '</a>';
 				echo "<br/> ";
 			}
-
 			echo "</div>\n";
 		}
 	?>
-	<h3><?= $t['title'];?></h3>
-
-	<div name="upper" filter="debug/debugHtml text/uc" class="content_wrapper">
-	<?php if($t['caption'] != '') {  ?>
-	<span style="padding-left:1em;font-size:90%;"><?= $t['caption'];?></span> 
-	<?php }  ?>
-
-
-	<?php echo $t['content'];?>
-	</div>   
-
+	
 	<?php
-		if ($t['hasPages']) {
+	// COMMENTED THIS OUT IN ORDER TO USE THE PAGE STYLE CODE ABOVE - SCOTT
+	/* 	if ($t['hasPages']) {
 			echo '<a href="';
 			echo cgn_appurl(
 				'main','
@@ -67,5 +76,9 @@
 			}
 			echo implode('&nbsp;|&nbsp;', $pagesArray);
 		}
+	*/
+	// THIS CODE WAS COMMENTED OUT TO ALLOW TO USE THE TITLE VERSION CODE
+	// WANTED TO HAVE THE TITLES AT THE BOTTOM OF THE PAGE FOR EACH PAGE BREAK
+	// ALLOWS ME TO CREATE NEW LABELS FOR SECTIONS OF A LONG ARTICLE.....SCOTT
 	?>
 
