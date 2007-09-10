52fe6becc65fcb4f0d5cca72d24ccda0381acc7d
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-09-10 14:32:04 +0000

    typo, removed extra comma in sql table install file
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@696 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- setup/db_install/cgn_content_rel.mysql.sql
+++ setup/db_install/cgn_content_rel.mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `cgn_content_rel` (
 	  `from_id` int(10) unsigned NOT NULL default '0',
-	  `to_id` int(10) unsigned NOT NULL default '0',
+	  `to_id` int(10) unsigned NOT NULL default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1; 
 
 CREATE INDEX `from_idx` ON `cgn_content_rel` (`from_id`);
