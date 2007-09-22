56ef292982b198de9251f2f1b92c3dc435ef29af
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-09-22 16:54:41 +0000

    Simple view into the object trash.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@725 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/admin/site/garbage.php
@@ -0,0 +1,46 @@
+<?php
+include_once('../cognifty/lib/html_widgets/lib_cgn_widget.php');
+include_once('../cognifty/lib/html_widgets/lib_cgn_toolbar.php');
+include_once('../cognifty/lib/lib_cgn_mvc.php');
+include_once('../cognifty/lib/lib_cgn_mvc_table.php');
+
+
+class Cgn_Service_Site_Garbage extends Cgn_Service_AdminCrud {
+
+	function Cgn_Service_Site_Garbage () {
+
+	}
+
+
+	function mainEvent(&$req, &$t) {
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query('select * from cgn_obj_trash');
+
+		$list = new Cgn_Mvc_TableModel();
+
+		//cut up the data into table data
+		while ($db->nextRecord()) {
+			$list->data[] = array(
+				$db->record['table'],
+				cgn_adminlink('restore','site','garbage','undo', array('undo_id'=>$db->record['cgn_obj_trash_id'], 'table'=>$db->record['table'])),
+				cgn_adminlink('erase','site','garbage','erase',array('cgn_obj_trash_id'=>$db->record['cgn_obj_trash_id'], 'table'=>'cgn_obj_trash'))
+			);
+		}
+		$list->headers = array('Table','Restore','Erase');
+
+		$t['dataGrid'] = new Cgn_Mvc_AdminTableView($list);
+
+	}
+
+	function eraseEvent(&$req, &$t) {
+
+		$id = $req->cleanInt('cgn_obj_trash_id');
+
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query('delete from cgn_obj_trash WHERE cgn_obj_trash_id = '.$id.' LIMIT 1');
+
+		$this->redirectHome($t);
+	}
+}
+
+?>
