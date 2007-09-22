b9395f9f22cf283b6766b2bb4fbdcc2828e28d94
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-09-22 17:57:18 +0000

    Update schema
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@730 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/install/main.php
+++ src/cognifty/modules/install/main.php
@@ -71,7 +71,7 @@ class Cgn_Service_Install_Main extends Cgn_Service {
 		$db = Cgn_Db_Connector::getHandle();
 
 		$thisdir = dirname(__FILE__);
-		for ($x=1; $x <= 15; $x++) {
+		for ($x=1; $x <= 24; $x++) {
 			$installTableSchemas = array();
 			@include($thisdir.'/sql/schema_'.sprintf('%02d',$x).'.php');
 			if (count($installTableSchemas)<1 ) {
