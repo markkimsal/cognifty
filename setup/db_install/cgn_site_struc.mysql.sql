e79d614c5513125650a1c79d7e451f2962d61bb1
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-12-01 02:59:06 +0000

    Fix names of site structure table.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@760 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- setup/db_install/cgn_site_struc.mysql.sql
+++ setup/db_install/cgn_site_struc.mysql.sql
@@ -3,19 +3,19 @@
 -- DB type: mysql
 
 
-DROP TABLE IF EXISTS `cgn_site_struc`;
-CREATE TABLE `cgn_struct` (
+DROP TABLE IF EXISTS `cgn_site_struct`;
+CREATE TABLE `cgn_site_struct` (
 		
-	`cgn_struct_id` integer (10) unsigned NOT NULL auto_increment, 
+	`cgn_site_struct_id` integer (10) unsigned NOT NULL auto_increment, 
 	`node_id` integer (10) unsigned NOT NULL default 0, 
 	`parent_id` integer (10) unsigned NOT NULL default 0, 
 	`node_kind` char    (10)  NOT NULL default 'web', 
 	`title`     varchar (255) NOT NULL, 
-	PRIMARY KEY (cgn_struct_id) 
+	PRIMARY KEY (cgn_site_struct_id) 
 );
 
-CREATE INDEX `node_idx`        ON cgn_struct (`node_id`);
-CREATE INDEX `parent_idx`      ON cgn_struct (`parent_id`);
-CREATE INDEX `node_kind_idx`   ON cgn_struct (`node_kind`);
+CREATE INDEX `node_idx`        ON cgn_site_struct (`node_id`);
+CREATE INDEX `parent_idx`      ON cgn_site_struct (`parent_id`);
+CREATE INDEX `node_kind_idx`   ON cgn_site_struct (`node_kind`);
 
-ALTER TABLE `cgn_site_struc` COLLATE utf8_general_ci;
+ALTER TABLE `cgn_site_struct` COLLATE utf8_general_ci;
