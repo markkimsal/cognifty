2abda7a5b4f0268a995c6054ea9a1cdfd5c840e2
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2007-12-25 04:51:23 +0000

    Moving the wikilayout into the shared app-lib dir.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@776 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/main.php
+++ src/cognifty/admin/content/main.php
@@ -5,7 +5,7 @@ include_once(CGN_LIB_PATH.'/lib_cgn_mvc.php');
 include_once(CGN_LIB_PATH.'/lib_cgn_mvc_table.php');
 
 include_once(CGN_LIB_PATH.'/form/lib_cgn_form.php');
-include_once(CGN_SYS_PATH.'/admin/content/wiki_form.php');
+include_once(CGN_SYS_PATH.'/app-lib/form/wikilayout.php');
 
 
 class Cgn_Service_Content_Main extends Cgn_Service_Admin {
