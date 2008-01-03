1f4810654a7bc909d9c56ab01c7523fb5a280f28
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-01-03 04:33:18 +0000

    Adding dates and spacing to blog rendering.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@797 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/blog/templates/main_main.html.php
+++ src/cognifty/modules/blog/templates/main_main.html.php
@@ -1,8 +1,25 @@
 <?
 
 foreach ($t['entries'] as $key=>$entry) {
+	$published = explode(' ',date('F d Y',$entry->posted_on));
+	$published['month'] = $published[0];
+	$published['date'] = $published[1];
+	$published['year'] = $published[2];
+?>
+	<div style="text-align:center;float:left;margin-top:0.5em;padding-right:1.5em;">
+		<span style="font-size:90%;">
+		<?=$published['month'];?>
+		</span>
+		<br/>
+		<span style="font-size:150%;">
+		<?=$published['date'];?>
+		</span>
+	</div>
+<?
 	echo '<h3>'.$entry->title.'</h3>';
 	echo "<hr/>\n";
 	echo $entry->content;
+	echo "<br/>\n";
+	echo "<br/>\n";
 }
 ?>
