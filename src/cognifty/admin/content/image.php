cd85b5cc8d53754f8a4e76f628af3bf924a4eae8
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-02-21 21:27:12 +0000

    adding new logic and icons to images panel
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@927 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/image.php
+++ src/cognifty/admin/content/image.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once(CGN_LIB_PATH.'/html_widgets/lib_cgn_widget.php');
+include_once(CGN_LIB_PATH.'/html_widgets/lib_cgn_toolbar.php');
 include_once(CGN_LIB_PATH.'/lib_cgn_mvc.php');
 include_once(CGN_LIB_PATH.'/lib_cgn_mvc_table.php');
 
@@ -17,52 +18,67 @@ class Cgn_Service_Content_Image extends Cgn_Service_AdminCrud {
 
 	function mainEvent(&$sys, &$t) {
 
-		$t['message1'] = '<h3>Images</h3>';
+		$t['message1'] = '<span style="font-size:120%;">Images</span>';
+
+		$t['toolbar'] = new Cgn_HtmlWidget_Toolbar();
+		$btn1 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','upload'),"New Image");
+		$t['toolbar']->addButton($btn1);
+		// GENERIC EXAMPLE OF HOW TO ADD ANOTHER BUTTON
+		// $btn2 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','web','new', array('mime'=>'wiki')),"New Wiki Page");
+		// $t['toolbar']->addButton($btn2);
+
+
 	
 		$db = Cgn_Db_Connector::getHandle();
-// SCOTTCHANGE - CONVERTED TO A RIGHT JOIN SO ONLY RECORDS IN PUBLISH WILL BE RETRIEVED- NOT CONTENT
-//		$db->query('select * from cgn_image_publish ORDER BY title');
-//		$db->query('SELECT A.title, A.cgn_content_id, A.published_on, B.cgn_image_publish_id
-//				FROM cgn_content AS A
-//				 LEFT JOIN cgn_image_publish AS B
-//					ON A.cgn_content_id = B.cgn_content_id
-//				WHERE sub_type = "image" 
-//			   	ORDER BY title');
-
-		$db->query('SELECT A.title, A.cgn_content_id, A.published_on, B.cgn_image_publish_id
+		$db->query('SELECT A.title, A.cgn_content_id, A.version, A.published_on, B.cgn_image_publish_id, B.cgn_content_version
 				FROM cgn_content AS A
 				LEFT JOIN cgn_image_publish AS B
 					ON A.cgn_content_id = B.cgn_content_id
 				WHERE sub_type = "image" 
 			   	ORDER BY title');
 
-
-
 		$list = new Cgn_Mvc_TableModel();
 
 		//cut up the data into table data
 		while ($db->nextRecord()) {
 			if ($db->record['published_on']) {
-				$published = '<img src="'.cgn_url().'/icons/default/bool_yes_24.png">';
+				$status = '<img src="'.cgn_url().
+				'/icons/default/bool_yes_24.png">';
+
+				if ($db->record['version']==$db->record['cgn_content_version']) {
+					$status = '<img src="'.cgn_url().
+					'/icons/default/bool_yes_24.png">';
+				} else {
+					$status = '<img src="'.cgn_url().
+					'/icons/default/caution_24.png">';
+				}
+				
 				$preview = '<img src="'.cgn_adminurl('content','preview','showImage',array('id'=>$db->record['cgn_image_publish_id'])).'"/>'; 
+				
 				$delLink =
 				cgn_adminlink('unpublish','content','image','del',array('cgn_image_publish_id'=>$db->record['cgn_image_publish_id'], 'table'=>'cgn_image_publish'));
+			
 			} else {
-				$published = '';
+		
+				$status = '';
+				
 				$preview = '<img src="'.cgn_adminurl('content','preview','showImage',array('cid'=>$db->record['cgn_content_id'])).'" height="64" border="1"/>'; 
+				
 				$delLink = cgn_adminlink('delete','content','image','del',array('cgn_content_id'=>$db->record['cgn_content_id'], 'table'=>'cgn_content'));
 			}
 
+
+
 			$list->data[] = array(
 				cgn_adminlink($db->record['title'],'content','view','',array('id'=>$db->record['cgn_content_id'])),
-				$published,
+				$status,
 				$preview,
 //				cgn_adminlink('edit','content','edit','',array('id'=>$db->record['cgn_content_id'])),
 				$delLink
 			);
 		}
 		// __FIXME__ add in editing capabilities.
-		$list->headers = array('Title','Published','Preview','Delete');
+		$list->headers = array('Title','Status','Preview','Delete');
 		//$list->headers = array('Title','Preview','Edit','Delete');
 
 		$t['menuPanel'] = new Cgn_Mvc_AdminTableView($list);
