11c4b13e20af2072f67b3562b1d8606d591d883c
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-02-21 21:26:55 +0000

    adding new logic and icons to assets panel
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@926 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/assets.php
+++ src/cognifty/admin/content/assets.php
@@ -1,8 +1,12 @@
 <?php
 
 include_once(CGN_LIB_PATH.'/html_widgets/lib_cgn_widget.php');
+include_once(CGN_LIB_PATH.'/html_widgets/lib_cgn_toolbar.php');
 include_once(CGN_LIB_PATH.'/lib_cgn_mvc.php');
 include_once(CGN_LIB_PATH.'/lib_cgn_mvc_table.php');
+
+include_once(CGN_SYS_PATH.'/app-lib/lib_cgn_content.php');
+
 
 
 class Cgn_Service_Content_Assets extends Cgn_Service_AdminCrud {
@@ -12,28 +16,90 @@ class Cgn_Service_Content_Assets extends Cgn_Service_AdminCrud {
 
 	function mainEvent(&$sys, &$t) {
 
-		$t['message1'] = '<h3>Site Assets</h3>';
+		$t['message1'] = '<span style="font-size:120%;">Assets</span>';
+
+		$t['toolbar'] = new Cgn_HtmlWidget_Toolbar();
+		$btn1 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','upload'),"New File");
+		$t['toolbar']->addButton($btn1);
+		// GENERIC EXAMPLE OF HOW TO ADD ANOTHER BUTTON
+		// $btn2 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','web','new', array('mime'=>'wiki')),"New Wiki Page");
+		// $t['toolbar']->addButton($btn2);
+
 	
 		$db = Cgn_Db_Connector::getHandle();
-		$db->query('select * from cgn_file_publish ORDER BY title');
+		$db->query('SELECT A.title, A.cgn_content_id, A.version, A.published_on, B.cgn_file_publish_id, B.cgn_content_version
+				FROM cgn_content AS A
+				LEFT JOIN cgn_file_publish AS B
+					ON A.cgn_content_id = B.cgn_content_id
+				WHERE sub_type = "file" 
+			   	ORDER BY title');
+
 
 		$list = new Cgn_Mvc_TableModel();
 
 		//cut up the data into table data
 		while ($db->nextRecord()) {
+
+			if ($db->record['published_on']) {
+				$status = '<img src="'.cgn_url().
+				'/icons/default/bool_yes_24.png">';
+				if ($db->record['version']==$db->record['cgn_content_version']) {
+					$status = '<img src="'.cgn_url().
+					'/icons/default/bool_yes_24.png">';
+				} else {
+					$status = '<img src="'.cgn_url().
+					'/icons/default/caution_24.png">';
+				}
+
+			} else {
+				$status = '';
+			}
+
+			$fileDescription = $db->record['description'];
+
+			if ($db->record['cgn_file_publish_id'] ) {
+				$delLink = cgn_adminlink('unpublish','content','assets','del',array('cgn_file_publish_id'=>$db->record['cgn_file_publish_id'], 'table'=>'cgn_file_publish'));
+			} else {
+				$delLink = cgn_adminlink('delete','content','assets','del',array('cgn_content_id'=>$db->record['cgn_content_id'], 'table'=>'cgn_content'));
+			}
+
 			$list->data[] = array(
 				cgn_adminlink($db->record['title'],'content','view','',array('id'=>$db->record['cgn_content_id'])),
-				$db->record['description'],
-//				cgn_adminlink('edit','content','edit','',array('id'=>$db->record['cgn_content_id'])),
-				cgn_adminlink('delete','content','assets','del',array('cgn_file_publish_id'=>$db->record['cgn_file_publish_id'], 'table'=>'cgn_file_publish'))
+				$status,
+				$fileDescription,
+				$delLink
 			);
+
 		}
 		// __FIXME__ add in edit capabilities
-		$list->headers = array('Title','Description','Delete');
-		$list->headers = array('Title','Description','Edit','Delete');
+		// $list->headers = array('Title','Description','Delete');
+		$list->headers = array('Title','Status','Description','Delete');
 
 		$t['menuPanel'] = new Cgn_Mvc_AdminTableView($list);
 	}
+
+	/**
+	 * Override this event so that we can unset the published_on date
+	 * in the content table.
+	 */
+	function delEvent(&$req, &$t) {
+		$table = $req->cleanString('table');
+		$id    = $req->cleanInt($table.'_id');
+		if ($table != 'cgn_file_publish') {
+			return parent::delEvent($req,$t);
+		}
+
+		//this is removing a asset publish record, basically an "unpublish" event
+		$asset = new Cgn_Asset($id);
+		$contentId = $asset->getContentId();
+		$content = new Cgn_Content($contentId);
+		$content->dataItem->published_on = 0;
+		$content->save();
+
+		$table = $req->cleanString('table');
+		return parent::delEvent($req,$t);
+	}
+
 }
 
 ?>
