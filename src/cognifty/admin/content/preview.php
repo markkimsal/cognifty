ff6f983469ccb1006e8c8205a10ba759623e7ed0
scottgilvin <scottgilvin@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-02-24 14:29:52 +0000

    patching functions that automatically embed links in content
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@951 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/preview.php
+++ src/cognifty/admin/content/preview.php
@@ -26,6 +26,7 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 
 	
 			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertImage(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\');\',300);" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+
 			$str .= '<img height="60" src="'.cgn_adminurl('content','preview','showImage',array('id'=>$db->record['cgn_image_publish_id'])).'" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
 			$str .= $db->record['title'].'</div>';
 			$t['data'][] = $str;
@@ -34,7 +35,7 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 
 	function browseFilesEvent(&$req, &$t) {
 
-		$this->templateName = 'preview_browseArticles';
+		$this->templateName = 'preview_browseFiles';
 		$db = Cgn_Db_Connector::getHandle();
 		$db->query('select * from cgn_file_publish ORDER BY title');
 
@@ -43,7 +44,8 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 		//cut up the data into table data
 		while ($db->nextRecord()) {
 
-			$str = '<div onclick="parent.insertFile(\''.$db->record['link_text'].'\',\''.$db->record['title'].'\',\''.$db->record['cgn_content_id'].'\');" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertFile(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['title'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\');\',300);" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+
 			$str .= '<img src="'.cgn_url().'media/icons/default/document.png" align="left"/>';
 			$str .= $db->record['title'].'</div>';
 			$t['data'][] = $str;
@@ -61,7 +63,8 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 		//cut up the data into table data
 		while ($db->nextRecord()) {
 
-			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertArticle(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['title'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\')\');" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertArticle(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['title'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\');\',300);" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+
 			$str .= '<img src="'.cgn_url().'media/icons/default/document.png" align="left"/>';
 			$str .= $db->record['title'].'</div>';
 			$t['data'][] = $str;
@@ -79,7 +82,8 @@ class Cgn_Service_Content_Preview extends Cgn_Service_Admin {
 		//cut up the data into table data
 		while ($db->nextRecord()) {
 
-			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertPage(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['title'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\')\');" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+			$str = '<div onclick="parent.$(\'#container-1 ol\').tabsClick(1);parent.$(\'#content\').focus();window.setTimeout(\'parent.insertPage(\\\''.$db->record['link_text'].'\\\',\\\''.$db->record['title'].'\\\',\\\''.$db->record['cgn_content_id'].'\\\');\',300);" style="cursor:pointer;float:left;text-align:center;margin-right:13px;">';
+
 			$str .= '<img src="'.cgn_url().'media/icons/default/html.png" align="left"/>';
 			$str .= $db->record['title'].'</div>';
 			$t['data'][] = $str;
