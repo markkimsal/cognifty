e3a761971b4d9354c106e526c2784363a20bc47b
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-03-24 14:17:38 +0000

    simple utility method to get the 5 latest blog posts.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@999 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/modules/blog/lib/Blog_Util.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Blog_Util {
+
+
+	function showRecentPosts() {
+		$entry = new Cgn_DataItem('cgn_blog_entry_publish');
+		$entry->orderBy('posted_on');
+		$entry->limit(5);
+		$posts = $entry->find();
+		foreach ($posts as $entry) {
+			echo '<li><a href="'.cgn_appurl('blog','entry','', array('id'=>$entry->cgn_blog_entry_publish_id)).$entry->link_text.'">'.$entry->title.'</a></li>';
+		}
+	}
+}
