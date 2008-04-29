9cbfb44ff189e5204386bbb97395f196d16a06de
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-04-29 21:48:20 +0000

    add external account reference id and number.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1021 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/account/setup/cgn_account.mysql.sql
+++ src/cognifty/modules/account/setup/cgn_account.mysql.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS `cgn_account`;
 CREATE TABLE `cgn_account` (
-		
+
 	`cgn_account_id` integer (11) unsigned NOT NULL auto_increment, 
 	`cgn_user_id` integer (11) unsigned NOT NULL, 
 	`firstname` varchar (100) NOT NULL default '',
 	`lastname` varchar (100) NOT NULL default '',
 	`title` varchar (12) NOT NULL default '',
-	`birthDate` integer (11) NOT NULL default '0',
+	`birth_date` integer (11) NOT NULL default '0',
 	PRIMARY KEY (`cgn_account_id`) 
 );
 
