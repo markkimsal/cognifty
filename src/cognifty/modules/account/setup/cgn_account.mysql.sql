d6cbd1ef6661992346099a1294c7e91eb3bd817b
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-04-30 01:24:24 +0000

    Adding extra contact email for accounts.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1024 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/account/setup/cgn_account.mysql.sql
+++ src/cognifty/modules/account/setup/cgn_account.mysql.sql
@@ -5,6 +5,7 @@ CREATE TABLE `cgn_account` (
 	`cgn_user_id` integer (11) unsigned NOT NULL, 
 	`firstname` varchar (100) NOT NULL default '',
 	`lastname` varchar (100) NOT NULL default '',
+	`contact_email` varchar (200) NOT NULL default '',
 	`title` varchar (12) NOT NULL default '',
 	`birth_date` integer (11) NOT NULL default '0',
 	`ref_id` varchar (50) NOT NULL default '',
