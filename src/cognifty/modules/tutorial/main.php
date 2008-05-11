edd9bdb8dce41f43a60d33e7dc76920676bb3744
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-05-11 16:37:43 +0000

    fixup the tutorial URLs a bit.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1050 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/tutorial/main.php
+++ src/cognifty/modules/tutorial/main.php
@@ -8,16 +8,39 @@ class Cgn_Service_Tutorial_Main extends Cgn_Service {
 	}
 
 
-	function mainEvent(&$sys, &$t) {
+	function mainEvent(&$req, &$t) {
 	//	Cgn_Template::assignString('Message1','This is the main event!');
 	}
 
-	function pageEvent(&$sys, &$t) {
+	function pageEvent(&$req, &$t) {
 		//secure the input
-		$filename = basename($sys->getvars['p']);
+		if (isset($req->getvars['p'])) {
+			$filename = basename(@$req->getvars['p']);
+		} else {
+			$filename = basename(@$req->getvars[0]);
+		}
+		//fix old style URLs
+		$aliases = 
+			array ('concept1'=> 'Framework_Concepts.html'
+			);
+
+		if (in_array($filename, array_keys($aliases))) {
+			$filename = $aliases[$filename];
+		}
+
+		if (substr($filename,-5) === '.html') {
+			$filename = substr($filename,0, -5);
+		} else {
+			//sub-directory simulation
+			$filename = basename(@$req->getvars[1]);
+		}
+
 		//get our location
 		$modDir = Cgn_ObjectStore::getConfig('path://default/cgn/module');
-		$t['contents'] = file_get_contents($modDir.'/tutorial/tut/'.$filename.'.html');
+		$t['contents'] = @file_get_contents($modDir.'/tutorial/tut/'.$filename.'.html');
+		if(!$t['contents']) {
+			$t['contents'] = 'Sorry, file not found.';
+		}
 	}
 }
 
