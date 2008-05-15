9864d78ba37de6855dfc7ac8d816668f12114ba9
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-05-15 22:53:27 +0000

    reworking the active formatter.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1062 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_active_formatter.php
+++ src/cognifty/lib/lib_cgn_active_formatter.php
@@ -3,12 +3,14 @@
 
 class Cgn_ActiveFormatter {
 
-	var $_varType;
 	var $_varVal;
+	var $_varFmt = NULL;
 
-	function Cgn_ActiveFormatter($val='',$type='') {
+	function Cgn_ActiveFormatter($val='',$format='') {
 		$this->_varVal  = $val;
-		$this->_varType = $type;
+		if ($format !== '') {
+			$this->_varFmt = $format;
+		}
 	}
 
 
@@ -16,20 +18,97 @@ class Cgn_ActiveFormatter {
 
 		switch($type) {
 			case 'phone':
-				$p = str_replace('-','',$this->_varVal);
-				$p = str_replace('(','',$p);
-				$p = str_replace(')','',$p);
-				$p = str_replace(' ','',$p);
-				$prefix = substr($p,0,3);
-				$first = substr($p,3,3);
-				$second = substr($p,6,4);
-				return '('.$prefix.') '.$first.'-'.$second;
+				$clean = $this->cleanVar($this->_varVal);
+				$clean = $this->cleanSpaces($clean);
+				return Cgn_ActiveFormatter_Printer::printAsPhone(
+					$clean,
+					$this->_varFmt
+				);
 			break;
+			case 'email':
+				$clean = $this->cleanEmail($this->_varVal);
+				return Cgn_ActiveFormatter_Printer::printAsEmail(
+					$clean,
+					$this->_varFmt
+				);
+
+			break;
+		}
+		return $this->_varVal;
+	}
+
+	public function cleanVar($v) { 
+		return preg_replace('/([^[:alpha:]0-9[:space:]])+/u', '', $v);
+	}
+
+	public function cleanSpaces($v) { 
+		return preg_replace('/[[:space:]]+/u', '', $v);
+	}
+
+	public function cleanEmail($v) { 
+		return preg_replace('/([^[:alpha:]0-9[:space:]@\.])+/u', '', $v);
+	}
+
+}
+
+class Cgn_ActiveFormatter_Settings {
+
+	public static function getDefault($type) {
+		switch($type) {
+			case 'phone':
+				return '(%d) %d-%d';
+
+			case 'email-full':
+				return '%s <%s>';
+
+			case 'email':
+				return '%s';
 		}
 	}
 }
 
-//global $global_formatter;
-//$global_formatter = new Cgn_ActiveFormatter();
-//$global_formatter->init($t);
+class Cgn_ActiveFormatter_Printer {
+
+	public static function printAsPhone($clean,$format='') {
+		if ($format == '') {
+			$format = Cgn_ActiveFormatter_Settings::getDefault('phone');
+		}
+		$prefix = substr($clean,0,3);
+		$first = substr($clean,3,3);
+		$second = substr($clean,6,4);
+		return sprintf($format, $prefix, $first, $second);
+	}
+
+
+	public static function printAsEmail($clean,$format='') {
+		//full formatting has "Name <user@domain.com>"
+		if ($format == '') {
+			if (strstr($clean, ' ') !== FALSE) {
+				$format = Cgn_ActiveFormatter_Settings::getDefault('email-full');
+			} else {
+				$format = Cgn_ActiveFormatter_Settings::getDefault('email');
+			}
+		}
+
+		//find the email part, and the rest is the 'name' part
+		if (strstr($clean, ' ') !== FALSE) {
+			$fullName = '';
+			$email = '';
+			$parts = explode(' ',$clean);
+			foreach ($parts as $p) {
+				if (strstr($p,'@')) {
+					$email = $p;
+				} else {
+					if ($fullName !=='') {$fullName .= ' ';}
+					$fullName .= $p;
+				}
+			}
+			$toFormat = array($fullName, $email);
+		} else {
+			$toFormat = array($clean);
+		}
+
+		return vsprintf($format, $toFormat);
+	}
+}
 ?>
