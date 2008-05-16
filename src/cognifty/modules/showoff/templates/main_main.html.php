11a47fadd6b7f990a54e0a43f290f1d0e9e05c8d
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-05-16 03:34:16 +0000

    Adding vergabe to the sandbox module to explain its purpose.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1068 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/showoff/templates/main_main.html.php
+++ src/cognifty/modules/showoff/templates/main_main.html.php
@@ -1,10 +1,26 @@
-<a href="<?=cgn_appurl('showoff','main','format');?>">Formatting</a>
+<h3>Sandbox Module</h3>
+<p>
+This module is intended as a way for developers to see how Cognifty works.
+By tracing the code and viewing the output at the same time, a developer can get a 
+better understanding of how Cognifty libraries are intended to be used.
+This page will not make sense if you are simply viewing it on-line, without a running 
+installation of Cognifty.
+</p>
+
+<h3>Other Pages</h3>
+<ul>
+<li><a href="<?=cgn_appurl('showoff','main','format');?>">Formatting</a>
+</li>
+<li><a href="<?=cgn_appurl('showoff','db');?>">Database</a>
+</li>
+<li><a href="<?=cgn_appurl('showoff','wiki');?>">Wiki</a>
+</li>
+<li><a href="<?=cgn_appurl('showoff','tree');?>">Tree</a>
+</li>
+</ul>
+
+
 <br/>
-<a href="<?=cgn_appurl('showoff','db');?>">Database</a>
-<br/>
-<a href="<?=cgn_appurl('showoff','wiki');?>">Wiki</a>
-<br/>
-<a href="<?=cgn_appurl('showoff','tree');?>">Tree</a>
 
 
 <fieldset><legend>Menu Widget</legend>
