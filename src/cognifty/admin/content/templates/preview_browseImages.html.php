9d2d17b29b025243bbd00110f91e1a9c3f634ded
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-05-17 17:15:21 +0000

    align the links to embed an image in the admin panel.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1080 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/templates/preview_browseImages.html.php
+++ src/cognifty/admin/content/templates/preview_browseImages.html.php
@@ -1,9 +1,11 @@
 <?php
 	foreach ($t['data'] as $datum) {
 ?>
-		<div style="float:left;text-align:left;margin-right:13px;color:#333;width:100%;">
-		<img height="60" src="<?=cgn_adminurl('content','preview','showImage',array('id'=>$datum['cgn_image_publish_id']));?>" style="float:left;text-align:center;margin-right:13px;">
+		<div style="float:left;text-align:left;color:#333;width:160px;">
+		<img height="60" src="<?=cgn_adminurl('content','preview','showImage',array('id'=>$datum['cgn_image_publish_id']));?>" style="float:left;text-align:center;margin-right:13px;"/>
 
+		</div>
+		<div style="text-align:left;margin-left:63px;color:#333;">
 		<a onclick="parent.$('#container-1 ol').tabsClick(1);parent.$('#content').focus();window.setTimeout('parent.insertImage(\'<?=$datum['link_text'];?>\',\'<?=$datum['cgn_content_id'];?>\');',300);"  href="#">Web Size Image</a>
 
 		<br/>
@@ -12,11 +14,10 @@
 		<br/>
 		<?=$datum['title'];?>
 		</div>
-		<br style=\"clear:both\"/>
+
+		<br style="clear:both"/>
 		<hr/>
 <?
 	}
 ?>
-
-
 
