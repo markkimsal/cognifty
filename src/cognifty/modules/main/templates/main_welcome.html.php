c00cc6b15ac82b13d0402fc17798ed4769e063da
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-05-21 01:17:07 +0000

    Add an explanation on the welcome page about no home page configured in the admin.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1113 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/main/templates/main_welcome.html.php
+++ src/cognifty/modules/main/templates/main_welcome.html.php
@@ -1,1 +1,10 @@
 Welcome to Cognifty.
+
+<p>
+Get access to the latest updates and news about Cognifty at <a href="http://cognifty.com/">cognifty.com</a>.
+</p>
+
+<p>
+This page appears when you have not created any particular home page.
+</p>
+
