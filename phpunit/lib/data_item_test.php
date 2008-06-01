13953e72ed2dfc008ad11e1c8c9a836700502917
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-06-01 17:58:44 +0000

    Adding data item test.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1192 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ phpunit/lib/data_item_test.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once(CGN_LIB_PATH.'/lib_cgn_db_connector.php');
+require_once(CGN_LIB_PATH.'/lib_cgn_db_mysql.php');
+
+class Cgn_DataItem_Test extends PHPUnit_Framework_TestCase {
+
+	function setUp() {
+		$db = Cgn_Db_Connector::getHandle();
+		$db->close();
+		$db->driverId = NULL;
+		$db->database = 'mysql';
+		$db->connect();
+		Cgn_DbWrapper::setHandle($db);
+	}
+
+
+	/**
+	 * Values as array should give back an array of only non-private vars
+	 */
+	function testValuesAsArray() {
+		$item = new Cgn_DataItem('user');
+		$item->andWhere('User', 'root');
+		$item->load();
+		$values = $item->valuesAsArray();
+		$this->assertGreaterThan(1, count($values));
+		$this->assertEqual('root', $values['User']);
+	}
+
+	function testPrimaryKey() {
+		$item = new Cgn_DataItem('random_table');
+		$item->setPrimaryKey(999);
+		$this->assertEqual( 999, $item->random_table_id);
+		$pkey = $item->getPrimaryKey();
+		$this->assertEqual( 999, $pkey);
+	}
+
+}
+?>
