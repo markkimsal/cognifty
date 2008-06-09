2681ad4d6c8dcc0238c3f195f7f6e2d364b9b015
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-06-09 13:19:07 +0000

    Refactor showRecentPosts method in blog utils.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1212 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/blog/lib/Blog_Util.php
+++ src/cognifty/modules/blog/lib/Blog_Util.php
@@ -1,16 +1,47 @@
 <?php
 
+Cgn::loadModLibrary('Blog::UserBlog','admin');
 
 class Blog_Util {
 
 
-	function showRecentPosts() {
-		$entry = new Cgn_DataItem('cgn_blog_entry_publish');
-		$entry->orderBy('posted_on DESC');
-		$entry->limit(5);
-		$posts = $entry->find();
+	/**
+	 * Echo an array of CGN data items to the screen.
+	 *
+	 * @param int number of posts to limit
+	 */
+	function showRecentPosts($limit=5) {
+		$posts = Blog_Util::getRecentPosts($limit);
 		foreach ($posts as $entry) {
 			echo '<li><a href="'.cgn_appurl('blog','entry','', array('id'=>$entry->cgn_blog_entry_publish_id)).$entry->link_text.'">'.$entry->title.'</a></li>';
 		}
+	}
+
+	/**
+	 * Return an array of CGN data items
+	 *
+	 * @param int number of posts to limit
+	 */
+	function getRecentPosts($limit=5) {
+		$limit = intval($limit);
+		$entry = new Cgn_DataItem('cgn_blog_entry_publish');
+		$entry->orderBy('posted_on DESC');
+		$entry->limit($limit);
+		return $entry->find();
+	}
+
+	/**
+	 * Return an array of Blog_UserBlog objects, array key is blog_id
+	 */
+	function getAllBlogs() {
+		$blogLoader = new Cgn_DataItem('cgn_blog');
+		$blogLoader->orderBy('title', 'ASC');
+		$blogList = $blogLoader->find();
+		//wrap each data item in a real Blog_UserBlog object
+		$userBlogs = array();
+		foreach ($blogList as $_item) {
+			$userBlogs[$_item->cgn_blog_id] = Blog_UserBlog::createObj($_item);
+		}
+		return $userBlogs;
 	}
 }
