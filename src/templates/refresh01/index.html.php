3552ec419919be422f5a5e98fcaa91998272360e
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-06-26 12:17:38 +0000

    Cleanup refresh template HTML.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1261 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/templates/refresh01/index.html.php
+++ src/templates/refresh01/index.html.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="<?=cgn_templateurl();?>images/Refresh.css" type="text/css" />
 <link rel="stylesheet" href="<?=cgn_url();?>media/shared_css/system.css" type="text/css" />
 
-<title><?php echo Cgn_Template::getPageTitle();?></title>
+<title><?php echo Cgn_Template::getPageTitle();?> :: <? cgn_sitename(); ?></title>
 </head>
 
 <body>
@@ -77,52 +77,17 @@
 				<a name="TemplateInfo"></a>
 				<h1>Template Info</h1>
 				
-				<p><strong>Refresh 1.0</strong> is a free, W3C-compliant, CSS-based website template 
-				by <strong><a href="http://www.styleshout.com/">styleshout.com</a></strong>. This work is 
-				distributed under the <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">
-				Creative Commons Attribution 2.5  License</a>, which means that you are free to 
-				use and modify it for any purpose. All I ask is that you include a link back to  
-				<a href="http://www.styleshout.com/">my website</a> in your credits.</p>  
-
 				<p>For more free designs, you can visit 
 				<a href="http://www.styleshout.com/">my website</a> to see 
 				my other works.</p>
 		
-				<p>Good luck and I hope you find my free templates useful!</p>
-				
 				<p class="post-footer align-right">					
 					<a href="index.html" class="readmore">Read more</a>
 					<a href="index.html" class="comments">Comments (7)</a>
 					<span class="date">Oct 01, 2006</span>	
 				</p>
 			
-				<a name="SampleTags"></a>
-				<h1>Sample Tags</h1>
-				
-				<h3>Code</h3>				
-				<p><code>
-				code-sample { <br />
-				font-weight: bold;<br />
-				font-style: italic;<br />				
-				}		
-				</code></p>	
-				
-				<h3>Example Lists</h3>
 			
-				<ol>
-					<li><span>example of ordered list</span></li>
-					<li><span>uses span to color the numbers</span></li>								
-				</ol>	
-							
-				<ul>
-					<li><span>example of unordered list</span></li>
-					<li><span>uses span to color the bullets</span></li>								
-				</ul>				
-				
-				<h3>Blockquote</h3>			
-				<blockquote><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy 
-				nibh euismod tincidunt ut laoreet dolore magna aliquam erat....</p></blockquote>
-				
 				<h3>Image and text</h3>
 				<p><a href="http://getfirefox.com/"><img src="images/firefox-gray.jpg" width="100" height="120" alt="firefox" class="float-left" /></a>
 				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
@@ -133,22 +98,6 @@
 				In tristique orci porttitor ipsum. Aliquam ornare diam iaculis nibh. Proin luctus, velit pulvinar 
 				ullamcorper nonummy, mauris enim eleifend urna, congue egestas elit lectus eu est. 				
 				</p>
-								
-				<h3>Example Form</h3>
-				<form action="#">			
-				<p>			
-				<label>Name</label>
-				<input name="dname" value="Your Name" type="text" size="30" />
-				<label>Email</label>
-				<input name="demail" value="Your Email" type="text" size="30" />
-				<label>Your Comments</label>
-				<textarea rows="5" cols="5"></textarea>
-				<br />	
-				<input class="button" type="submit" />		
-				</p>		
-				</form>				
-				<br />	
-
 -->
 			<!-- site-content-main ends here -->	
 			</div>
