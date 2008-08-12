ae167535fd927bd79057958c5da9a24055cc1f9e
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-08-12 14:31:26 +0000

    Fixed throwError so that it doesn't count every error type as critical failures.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1413 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_error.php
+++ src/cognifty/lib/lib_cgn_error.php
@@ -193,7 +193,7 @@ class Cgn_ErrorStack {
 	 * from userspace doesn't cut it
 	 */
 	function throwError ($msg,$errNum,$type='error') {
-		$e = new Cgn_RuntimeError($msg,$errNum,0,$type);
+		$e = new Cgn_RuntimeError($msg,$errNum,$type);
 		$bt = debug_backtrace();
 		array_shift($bt);
 		$e->addBackTrace($bt);
