503c406a2555acdcbe47a679a04b6e3b29d2002d
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-09-03 22:17:19 +0000

    Added a simple mail class
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1455 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/lib/mail/lib_cgn_message_mail.php
@@ -0,0 +1,80 @@
+<?php
+
+class Cgn_Message_Mail {
+
+	var $body;
+	var $subject;
+	var $toList         = array();
+	var $from           = '';
+	var $reply           = '';
+	var $ccList         = array();
+	var $bccList        = array();
+
+	//behavioral settings
+	var $sendCombinedTo = TRUE;
+	var $useSmtp        = FALSE;
+
+
+	/**
+	 * Wrap php's mail function.
+	 *
+	 * If sendCombinedTo is set to false, this will send 1 mail for each
+	 * entry in $this->toList, else it will combine all entries into 1 line.
+	 */
+	function sendMail() {
+		$errno = 0;
+		$errstr = '';
+
+		$headers = $this->createExtraHeaders();
+		if ($this->sendCombinedTo == TRUE) {
+			$toList = $this->combineToList();
+		} else {
+			foreach ($this->toList as $to) {
+				if ($this->useSmtp)
+				mail ($to, $this->subject, $this->body, $headers);
+			}
+			return TRUE;
+		}
+		return mail($toList, $this->subject, $this->body, $headers);
+	}
+
+
+	/**
+	 * return a comma separated list of to addresses
+	 */
+	function combineToList() {
+		return implode(', ', $this->toList);
+	}
+
+	function createExtraHeaders() {
+		$headers = array();
+
+		$this->makeFromHeader($headers);
+
+		$this->makeReplyHeader($headers);
+
+		$headers2 = array();
+
+
+		foreach ($headers as $h => $v) {
+			$v = str_replace("\n", '', $v);
+			$v = str_replace("\r", '', $v);
+
+			$headers2[]  = $h.': '.trim($v);
+		}
+
+		return implode("\n", $headers2);
+	}
+
+	function makeFromHeader(&$h) {
+		if ($this->from != '')
+		$h['From'] = $this->from;
+	}
+
+	function makeReplyHeader(&$h) {
+		if ($this->reply != '')
+		$h['Reply-to'] = $this->reply;
+	}
+}
+
+?>
