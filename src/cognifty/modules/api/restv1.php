f36f79f9f114256464b3a6d8843c718ca5e874c7
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-09-17 15:39:47 +0000

    Adding basic API module to handle agent requests.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1473 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/modules/api/restv1.php
@@ -0,0 +1,77 @@
+<?php
+
+include_once(CGN_SYS_PATH.'/app-lib/lib_cgn_content.php');
+
+class Cgn_Service_Api_Restv1 extends Cgn_Service {
+
+	var $crumbs = NULL;
+
+	function Cgn_Service_Api_Restv1 () {
+	}
+
+
+	/**
+	 * Called before any events.
+	 *
+	 * If this call fails, no more processing will continue;
+	 *
+	 * Overridden to change the default user for one specified by the API KEY
+	 */
+	function init($req, $mod, $srv, $evt) { 
+		global $cgnUser;
+		parent::init($req, $mod, $srv, $evt);
+
+		$u = $req->getUser();
+		$req->isAjax = TRUE;
+		//don't accept logged in users, that means
+		//they have a cookie, or this session is already 
+		//bound to someone else
+		if (!$u->isAnonymous()) {
+			return FALSE;
+		}
+
+		$key = $req->cleanString('key');
+		if ($key == '') {
+			return FALSE;
+		}
+		$item = new Cgn_DataItem('cgn_user');
+		$item->andWhere('enable_agent', 1);
+		$item->andWhere('agent_key', $key);
+		$item->load();
+
+		if ($item->_isNew) { //load failed
+			return FALSE;
+		}
+
+		$u = Cgn_User::load($item->cgn_user_id);
+		$u->loadGroups();
+		$cgnUser = $u;
+		return TRUE;
+	}
+
+	/**
+	 * Change the current user based on the API KEY, then emit  
+	 * a signal for the current event.
+	 */
+	function processEvent($e,&$req,&$t) {
+		/*
+		$newTicket = new Cgn_SystemTicket('rss');
+		Cgn_SystemRunner::stackTicket($newTicket);
+		 */
+
+		/*
+		$u = $req->getUser();
+		$t['message'] = 'Emitting signal api_'.$e;
+		$t['message2'] = 'You username is  '.$u->username;
+		*/
+		$this->presenter = 'self';
+		$this->req = $req;
+		$this->t   = $t;
+		$this->emit('api_'.$e);
+	}
+
+	function output($req, &$t) {
+		echo json_encode($t);
+	}
+}
+?>
