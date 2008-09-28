5045428cdbb6bbb8001506839b5bd8181a0872c7
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-09-28 16:45:38 +0000

    Added simple benchmark module.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1495 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/modules/benchmark/main.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Cgn_Service_Benchmark_Main extends Cgn_Service {
+
+	/**
+	 * Load up a number of articles and display them.
+	 * Only show the first bit of the text and the author.
+	 * This should be highly configurable from a "front-page"
+	 * manager in the admin section.
+	 */
+	function mainEvent(&$sys, &$t) {
+		$this->crumbs[] = "About";
+		$this->presenter = 'self';
+		$page = new Cgn_DataItem('pages');
+		$pages = $page->find();
+	}
+
+	function output(&$sys, &$t) {
+		include('cognifty/modules/benchmark/templates/main.html.php');
+	}
+}
+
+?>
---
+++ src/cognifty/modules/benchmark/templates/main.html.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Framework Test</title>
+
+<link href="main.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <div id="page"> 
+    
+        <div id="header">
+            <h1>Framework Test</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+            </ul>
+        </div>
+
+        <div id="content">
+            <h2>Page List</h2>
+            <?php echo $content; ?>
+        </div>
+
+        <div id="footer">
+            <p>&copy; 2008 AVNet Labs.</p>
+        </div>
+
+    </div>
+</body>
+</html>
+
