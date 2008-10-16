0a6da89a6e99e106ee05656a6c73486d23def67f
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-10-16 15:45:22 +0000

    create cgn_sess if not exists
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1518 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- setup/upgrades/0006_0007/03_change_defaults.mysql.sql
+++ setup/upgrades/0006_0007/03_change_defaults.mysql.sql
@@ -55,6 +55,20 @@ ALTER TABLE `cgn_mxq_channel`
 #  Possibly data modifications needed!
 #
 
+
+CREATE TABLE IF NOT EXISTS `cgn_sess` (
+
+  `cgn_sess_id` integer (11) unsigned NOT NULL auto_increment,
+  `cgn_sess_key` varchar (100) NOT NULL,
+  `saved_on` int (11) NOT NULL default 0,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (cgn_sess_id)
+);
+
+CREATE INDEX cgn_sess_key_idx ON cgn_sess (cgn_sess_key);
+ALTER TABLE `cgn_sess` COLLATE utf8_collate_ci;
+
+
 ALTER TABLE `cgn_sess`
     ADD `saved_on` int(11) NOT NULL DEFAULT '0' AFTER cgn_sess_key,
     MODIFY `cgn_sess_key` varchar(100) NOT NULL DEFAULT '';
