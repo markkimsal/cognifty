84bb60d26372259d939b6682e0fd461e92388657
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-12-16 17:40:37 +0000

    Added simple database backup system.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1692 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

---
+++ src/cognifty/admin/backup/main.php
@@ -0,0 +1,231 @@
+<?php
+
+Cgn::loadLibrary('Html_Widgets::lib_cgn_widget'); //needed for widgets
+Cgn::loadLibrary('Html_Widgets::lib_cgn_toolbar'); //tool bar is a subclass of widget
+Cgn::loadLibrary('lib_cgn_mvc');    //all tables, lists, etc
+Cgn::loadLibrary('lib_cgn_mvc_table'); //table specific MVC pattern
+
+
+class Cgn_Service_Backup_Main extends Cgn_Service_AdminCrud {
+
+	public $pageTitle = 'Site Backups';
+
+	public function mainEvent($req, &$t) {
+		parent::mainEvent($req, $t);
+		$t['backupForm'] = $this->_loadBackupForm();
+	}
+
+
+	public function erEvent($req, &$t) {
+		$f = $req->cleanString('f');
+		$filename = BASE_DIR.'var/backups/'.$f;
+		`rm -f $filename`;
+		$this->redirectHome($t);
+	}
+
+
+	public function dlEvent($req, &$t) {
+		$t['f'] = $req->cleanString('f');
+		$t['filename'] = BASE_DIR.'var/backups/'.$req->cleanString('f');
+		$this->presenter = 'self';
+	}
+
+	public function doBackupEvent($req, &$t) {
+		$db = Cgn_Db_Connector::getHandle();
+		$this->_writeOutDbConnection($db);
+
+		$u = $req->getUser();
+		$u->addSessionMessage('Back-up Saved!');
+		$this->redirectHome($t);
+	}
+
+	//no toolbar for site backups
+	protected function _makeToolBar() {
+		return '';
+	}
+
+	/**
+	 * return non-associative array
+	 */
+	protected function _makeTableRow($_d) {
+		$dl = cgn_adminlink('Download', 'backup', 'main', 'dl', array('f'=>$_d['display_name']));
+		$er = cgn_adminlink('Erase',    'backup', 'main', 'er', array('f'=>$_d['display_name']));
+		return array($_d['display_name'], $dl, $er);
+	}
+
+	protected function _loadListData() {
+		$ret = array();
+		$this->_tryToMakeDir();
+		$d = dir(BASE_DIR.'var/backups');
+		if (!$d) {
+			return array();
+		}
+		while ($entry = $d->read()) {
+			if ($entry[0] == '.') continue;
+			$ret[] = array('display_name'=>$entry);
+		}
+		$d->close();
+		return $ret;
+	}
+
+	public function _tryToMakeDir() {
+		if (!file_exists(BASE_DIR.'var/backups')) {
+			 mkdir(BASE_DIR.'var/backups');
+		}
+	}
+
+	public function _loadBackupForm() {
+		Cgn::loadLibrary('Form::lib_cgn_form');
+		Cgn::loadLibrary('Html_Widgets::lib_cgn_widget');
+
+		$f = new Cgn_FormAdmin('content_01');
+		$f->action = cgn_adminurl($this->moduleName, $this->serviceName, 'doBackup');
+		$f->label = 'Create New Backup';
+		$checkElement = new Cgn_Form_ElementCheck('include_content', 'Include Content?');
+		$checkElement->addChoice('yes', 'yes', true);
+
+		$f->appendElement($checkElement);
+		$f->setShowCancel(FALSE);
+		return $f;
+	}
+
+	/**
+	 * Return false if the file could not be opened
+	 */
+	public function _writeOutDbConnection($db) {
+		$filename = BASE_DIR.'var/backups/cgn_backup_'.date('Ymd_Gis').'.sql';
+		$f = fopen($filename, 'w');
+		if (!$f) {
+			return false;
+		}
+//echo "<pre>";
+		$hdr  = "/* Dumped Database: ".date('Y-m-d G:i:s')." */\n";
+		$hdr .= "/* Schema Name: ".$db->database." */\n\n";
+		fwrite($f, $hdr, strlen($hdr));
+//echo $hdr;
+
+		$tables = $db->getTables();
+		foreach ($tables as $_tname => $_t) {
+			$this->_writeOutTable($_t, $db, $f);
+		}
+
+		fwrite($f, "\n\n", strlen("\n\n"));
+		fclose($f);
+
+		`mysqldump -t -u {$db->user} -p{$db->password} -h {$db->host} {$db->database} >> $filename`;
+		`gzip $filename`;
+	}
+
+
+	public function _writeOutTable($_t, $db, $f) {
+		$colDef = '`%s` %s %s %s';
+
+		$line = "\n/* Table Structure for: ".$_t." */\n";
+		$line .= "DROP TABLE IF EXISTS `".$_t."`;\n";
+		$line .= "Create TABLE `".$_t."` (\n";
+
+		fwrite($f, $line, strlen($line));
+//echo $line;
+
+		$_tstruct = $db->getTableColumns($_t);
+		$_idx     = $db->getTableIndexes($_t);
+
+		$names = $_tstruct['name'];
+		$types = $_tstruct['type'];
+		$lens  = $_tstruct['len'];
+		$flags = $_tstruct['flags'];
+		$def   = $_tstruct['def'];
+
+		$colLines = array();
+		foreach ($names as $_key => $_val) {
+			if ($lens[$_key] > 0) {
+				$thisLen = '('.$lens[$_key].')';
+			} else {
+				$thisLen = '';
+			}
+			$thisFlags = $this->_getFlags($flags[$_key]);
+			$thisFlags .= $this->_getDefault($def[$_key]);
+			$colLines[] = sprintf($colDef, $_val, $types[$_key], $thisLen, $thisFlags);
+		}
+
+		//if there's a Primary key index, add it to the table defintion
+		if (isset($_idx['PRIMARY'])) {
+			$pkey = $_idx['PRIMARY'][1]['column'];
+			$colLines[] = "PRIMARY KEY (`".$pkey."`)";
+			unset($_idx['PRIMARY']);
+		}
+
+		$line = "   ".implode(",\n   ", $colLines)."\n";
+			fwrite($f, $line, strlen($line));
+//echo $line;
+
+		$line = ");\n";
+		$line .= "ALTER TABLE `".$_t."` COLLATE utf8_general_ci;\n";
+		fwrite($f, $line, strlen($line));
+//echo $line;
+
+
+		$this->_writeOutIndex($_idx, $_t, $f);
+	}
+
+
+	/**
+	 * Write out special indexes as alter tables
+	 */
+	public function _writeOutIndex($idx, $_t, $f) {
+		$idxDef = 'CREATE %s INDEX `%s` ON `%s` (`%s`);';
+		$colLines = array();
+
+		foreach ($idx as $_idname => $_idst) {
+			$colNames = array();
+			foreach ($_idst as $_idcol) {
+				if ($_idcol['unique'] == '0') {
+					$unq = 'UNIQUE';
+				} else {
+					$unq = '';
+				}
+				$colNames[] = $_idcol['column'];
+			}
+			$colNames = implode('`,`', $colNames);
+			$colLines[] = sprintf($idxDef, $unq, $_idname, $_t, $colNames);
+		}
+		$line = implode("\n", $colLines)."\n";
+
+		fwrite($f, $line, strlen($line));
+//echo $line;
+	}
+
+	public function _getDefault($def) {
+		if (is_numeric($def)) {
+			return 'DEFAULT '.$def;
+		}
+
+		return 'DEFAULT "'.$def.'"';
+	}
+
+	public function _getFlags($f) {
+		$flg = '';
+		if (strpos($f, 'not_null') !== FALSE) {
+			$flg .= 'NOT NULL ';
+		} else {
+			$flg .= 'NULL ';
+		}
+		return $flg;
+		$f = trim(str_replace('blob', '', $f));
+		$f = trim(str_replace('binary', '', $f));
+		return trim(str_replace('multiple_key', '', $f));
+	}
+
+
+	public function output($req, $t) {
+		$f = fopen($t['filename'], 'r');
+		if (!$f) {
+			echo 'cannot open backup file.';
+			return;
+		}
+		header('Content-type: application/x-octet-stream');
+		header('Content-disposition: attachment;filename='.$t['f']);
+		fpassthru($f);
+		fclose($f);
+	}
+}
