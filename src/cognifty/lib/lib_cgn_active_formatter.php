9e0063974747d3d0a40d48166fa9c09470e46de4
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-12-16 14:51:02 +0000

    Some work done on active formatter (SILENT).
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1687 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_active_formatter.php
+++ src/cognifty/lib/lib_cgn_active_formatter.php
@@ -52,6 +52,93 @@ class Cgn_ActiveFormatter {
 		return $output;
 	}
 
+	function showAs($val, $type) {
+		$af = new Cgn_ActiveFormatter($val, $type);
+				//echo $af->_varType; echo 'l skdjf';
+		return $af->printAs($af->_varType);
+	}
+
+	function showComposed($val, $type) {
+		$output = '';
+		switch ($type) {
+		case 'CONTENT_ENTRY':
+		case 'CONTENT_ENTRY_LIST':
+			$output .= Cgn_ActiveFormatter_Title::getOutput($val);
+			$output .= Cgn_ActiveFormatter_Content::getOutput($val);
+			$output .= Cgn_ActiveFormatter_Content_Links::getOutput($val);
+			break;
+		}
+		return $output;
+	}
+
+	function show($val, $hint='') {
+		//guess that the value's type
+		if ($hint != '') {
+			$type = Cgn_ActiveFormatter::getTypeFromHint($hint);
+			$composed = Cgn_ActiveFormatter::isTypeComposed($type);
+			$list     = Cgn_ActiveFormatter::isTypeList($type);
+			if (!$composed) {
+				return Cgn_ActiveFormatter::showAs($val,$type);
+			} else {
+				if ($list) {
+					$output = '';
+					foreach ($val as $_listItem) {
+						$output .= Cgn_ActiveFormatter::showComposed($_listItem,$type);
+					}
+					return $output;
+				} else {
+					return Cgn_ActiveFormatter::showComposed($val,$type);
+				}
+			}
+		} else {
+			echo "hint is blank";
+		}
+
+	}
+
+	function getTypeFromHint($hint) {
+		switch ($hint) {
+			case 'phone': return 'PHONE';
+			case 'title': return 'TITLE';
+			case 'entry': return 'CONTENT_ENTRY';
+			case 'content': return 'CONTENT_ENTRY';
+			case 'articles': return 'CONTENT_ENTRY_LIST';
+			case 'blog_entry': return 'CONTENT_ENTRY_BLOG';
+		}
+		return 'UNKNOWN ('.$hint.')';
+	}
+
+
+	/**
+	 * A type is 'composed' if it is complex and contains other parts that need formatting separately.
+	 *
+	 * ex. A blog entry is composed of a title, some content, and some 'read more' links
+	 *     A phone number is not composed of anything.
+	 * @return bool type is composed if true
+	 */
+	function isTypeComposed($type) {
+		switch ($type) {
+			case 'PHONE': return false;
+			case 'TITLE': return false;
+			case 'CONTENT_ENTRY': return true;
+			case 'CONTENT_ENTRY_LIST': return true;
+			case 'CONTENT_ENTRY_BLOG': return true;
+		}
+		return false;
+	}
+
+	/**
+	 * A type is a 'list' if it is an array
+	 *
+	 * @return bool type is a list if true
+	 */
+	function isTypeList($type) {
+		switch ($type) {
+			case 'CONTENT_ENTRY_LIST': return true;
+		}
+		return false;
+	}
+
 	public function cleanVar($v) { 
 		return preg_replace('/([^[:alpha:]0-9[:space:]])+/u', '', $v);
 	}
@@ -124,6 +211,26 @@ class Cgn_ActiveFormatter_Printer {
 		}
 
 		return vsprintf($format, $toFormat);
+	}
+}
+//
+//
+
+class Cgn_ActiveFormatter_Title {
+	function getOutput($val){
+		return "<h2>".$val->title."</h2>\n";
+	}
+}
+
+class Cgn_ActiveFormatter_Content {
+	function getOutput($val){
+		return "<p>".$val->content."</p>\n";
+	}
+}
+
+class Cgn_ActiveFormatter_Content_Links {
+	function getOutput($val) {
+		return '<div class="links"><a href="'. cgn_appurl('main','content','').$val->link_text .'">Read More...</a> </div>';
 	}
 }
 ?>
