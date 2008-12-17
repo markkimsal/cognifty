0e21cb9b1dd47181ec73b58af2b3c053e46a40d7
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-12-17 21:16:48 +0000

    Add Backup links to system menu on admin template.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1702 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/templates/admin01/index.html.php
+++ src/templates/admin01/index.html.php
@@ -187,6 +187,7 @@ $(document).ready(function(){
 		<li><a href="<?=cgn_adminurl('mxq')?>">MXQ</a></li>
 		<li><a href="<?=cgn_adminurl('site','garbage')?>">Trash Can</a></li>
 		<li><a href="<?=cgn_adminurl('mods')?>">Modules</a></li>
+		<li><a href="<?=cgn_adminurl('backup')?>">Back-ups</a></li>
 		</ul>
 	</li>
 
