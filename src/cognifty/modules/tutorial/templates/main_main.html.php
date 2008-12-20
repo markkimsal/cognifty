bd0075f286f611fc28bd2ddd8218e36e9700a776
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2008-12-20 04:39:03 +0000

    Added tutorial page for migrating template themes to cognifty.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1704 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/tutorial/templates/main_main.html.php
+++ src/cognifty/modules/tutorial/templates/main_main.html.php
@@ -2,15 +2,22 @@
 <p>
 Get copy &amp; paste code samples from the <a href="<?=cgn_appurl('tutorial','cheatsheet');?>">Live Cheat Sheet</a>.
 </p>
-<h3>Cognifty Framework Tutorial</h3>
+
+<h3>Sandbox Module</h3>
+<p>
+Take a look at the <a href="<?=cgn_appurl('showoff');?>">sandbox module</a> for some ideas about how the included libraries work.</p>
+
+
+<h3>Cognifty Developers Guide</h3>
 
 <p>
 Did you ever want a Wiki site that could also sell products?  Or perhaps a CMS with an integrated forum...  Most systems aren't written with true application design as the number one goal.  Cognifty is different.  Cognifty is designed for the programmer who knows exactly the type of application they want to create but can't find anything quite right already made.
 </p>
+
 <ol>
 <li><a href="<?=cgn_appurl('tutorial','main','page').'Framework_Concepts.html';?>">Learn about Cognifty concepts</a></li>
 <li><a href="<?=cgn_appurl('tutorial','main','page').'How_to_Make_a_Module.html';?>">Learn how to make a module</a></li>
+<li><a href="<?=cgn_appurl('tutorial','main','page').'Customize_a_Module.html';?>">Learn how to customize the behavior of a module.</a></li>
+<li><a href="<?=cgn_appurl('tutorial','main','page').'Create_a_Template.html';?>">Learn how to activate any HTML template.</a></li>
 </ol>
 
-<p>
-Take a look at the <a href="<?=cgn_appurl('showoff');?>">sandbox module</a> for some ideas about how the included libraries work.</p>
