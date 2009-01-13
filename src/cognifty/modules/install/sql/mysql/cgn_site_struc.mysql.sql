5d830d44595ee91cdb0c3f724180d938998c34c2
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2009-01-13 00:30:32 +0000

    Adding link_text column to cgn_site_struct table.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1720 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/install/sql/mysql/cgn_site_struc.mysql.sql
+++ src/cognifty/modules/install/sql/mysql/cgn_site_struc.mysql.sql
@@ -11,12 +11,14 @@ CREATE TABLE `cgn_site_struct` (
 	`parent_id` integer (10) unsigned NOT NULL default 0, 
 	`node_kind` char    (10)  NOT NULL default 'web', 
 	`title`     varchar (255) NOT NULL, 
-	PRIMARY KEY (cgn_site_struct_id) 
+	`link_text` varchar (255) NOT NULL, 
+	PRIMARY KEY (`cgn_site_struct_id`) 
 );
 
 CREATE INDEX `node_idx`        ON `cgn_site_struct` (`node_id`);
 CREATE INDEX `parent_idx`      ON `cgn_site_struct` (`parent_id`);
 CREATE INDEX `node_kind_idx`   ON `cgn_site_struct` (`node_kind`);
 CREATE INDEX `title_idx`       ON `cgn_site_struct` (`title`);
+CREATE INDEX `link_text_idx`   ON `cgn_site_struct` (`link_text`);
 
 ALTER TABLE `cgn_site_struct` COLLATE utf8_general_ci;
