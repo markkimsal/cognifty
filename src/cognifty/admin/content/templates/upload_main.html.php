deff3908ac82f991e0fa68ccf75273fd7ab3fcf8
hardcorelamer <hardcorelamer@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2009-01-17 21:38:37 +0000

    Admin content upload: fixed javascript issues with FF3.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1734 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/templates/upload_main.html.php
+++ src/cognifty/admin/content/templates/upload_main.html.php
@@ -3,18 +3,29 @@ echo $t['form']->toHtml();
 ?>
 
 <script type="text/javascript">
-	 $(document).ready(function(){
-		  $("input[@type=file]").change(function(){
-			  if ($("#title").attr("value") == '') {
-			  	$("#title").attr("value", rightFromSubString( $("#filename").attr("value"), "/"));
-			  }
-				$("#description").focus();
-				  });
-	 });
+	$(document).ready(function(){
+		var lastfname = ''
+		//$("input[@type='file']").change(function(){
+		$("#filename").change(function(){
+			if ($("#title").attr("value") == lastfname ||
+				$("#title").attr("value") == '') {
+				var fname = $("#filename").attr("value");
+				if (fname.lastIndexOf('/') != -1) {
+					fname = rightFromSubString( fname, '/');
+				}
+				if (fname.lastIndexOf('\\') != -1) {
+					fname = rightFromSubString( fname, '\\');
+				}
+				lastfname = fname;
+				$("#title").attr("value", fname);
+			}
+			$("#description").focus();
+			});
+	});
 
 function rightFromSubString(fullString, subString) { 
 	if (fullString.lastIndexOf(subString) == -1) { 
-		return ""; 
+		return ''; 
 	} else { 
 		return fullString.substring(fullString.lastIndexOf(subString)+1, fullString.length); 
 	} 
