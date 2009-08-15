e14c66b45d86df5079befc4dcf7d52fb9ebdb645
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2009-08-15 17:40:57 +0000

    Admin: Combined 'homepage' features into 'web', simple house-cleaning. (SILENT)
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1918 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/content/web.php
+++ src/cognifty/admin/content/web.php
@@ -24,7 +24,7 @@ class Cgn_Service_Content_Web extends Cgn_Service_AdminCrud {
 		$t['toolbar']->addButton($btn2);
 		$btn3 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','web','republish'), "Mass Republish");
 		$t['toolbar']->addButton($btn3);
-		$btn4 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','homepage',''), "Set a Homepage");
+		$btn4 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','web', 'homepage'), "Set a Homepage");
 		$t['toolbar']->addButton($btn4);
 
 
@@ -162,6 +162,105 @@ class Cgn_Service_Content_Web extends Cgn_Service_AdminCrud {
 		$req->getUser()->addSessionMessage('Re-published '.$count.' Web pages.');
 		$this->redirectHome($t);
 	}
+
+	function homepageEvent(&$req, &$t) {
+
+		$this->displayName = 'Pages - Set a homepage';
+
+		$t['toolbar'] = new Cgn_HtmlWidget_Toolbar();
+		$btn2 = new Cgn_HtmlWidget_Button(cgn_adminurl('content','web',''), "Back to Pages");
+		$t['toolbar']->addButton($btn2);
+
+		$db = Cgn_Db_Connector::getHandle();
+		$db->query('SELECT A.title, A.cgn_content_id, A.version, A.published_on, B.cgn_web_publish_id, B.cgn_content_version, B.is_home
+				FROM cgn_content AS A
+				LEFT JOIN cgn_web_publish AS B
+					ON A.cgn_content_id = B.cgn_content_id
+				WHERE sub_type = "web" 
+			   	ORDER BY title');
+
+		$list = new Cgn_Mvc_TableModel();
+
+		//cut up the data into table data
+		while ($db->nextRecord()) {
+			if ($db->record['is_home']=='1') {
+				
+				// is the is_home field true (1)  ??
+				$status = '<img src="'.cgn_url().
+				'/media/icons/default/bool_yes_24.png">';
+				
+				// if it's true,(1), provide a link to unset this as a home page ??
+				$unsetLink = cgn_adminlink('clear','content','web','unsetHp',array('cgn_web_publish_id'=>$db->record['cgn_web_publish_id'], 'table'=>'cgn_web_publish'));
+			
+				// if it's tru, (1), leave blank, it is already set ??
+				$setLink = '';
+			
+			} else {
+			
+				// if it's false, anything other than (1), leave blank ??
+				$status = '';
+				
+				// if it's false, anything other than (1), leave blank, nothing to unset ??
+				$unsetLink = '';
+
+				// if it's false, anything other than (1), provide a link to set this as a home page ??
+				$setLink = cgn_adminlink('set','content', 'web','setHp',array('cgn_web_publish_id'=>$db->record['cgn_web_publish_id'], 'table'=>'cgn_web_publish'));			
+			}
+			
+			$list->data[] = array(
+				cgn_adminlink($db->record['title'],'content','view','',array('id'=>$db->record['cgn_content_id'])),
+				$status,
+				$unsetLink,
+				$setLink
+			);
+		}
+		
+		$list->headers = array('Title','Status','Clear HP Status','Set HP Status');
+
+		$t['menuPanel'] = new Cgn_Mvc_AdminTableView($list);
+	}
+
+	/**
+	 * Set the is_home data to '1'
+	 * in the cgn_web_publish table.
+	 */
+	function setHpEvent(&$req, &$t) {
+		$table = $req->cleanString('table');
+		$id    = $req->cleanInt($table.'_id');
+		if ($table != 'cgn_web_publish') {
+			return parent::setEvent($req,$t);
+		}
+
+		$db = Cgn_Db_Connector::getHandle();
+
+		$sqlQuery01 = 'UPDATE '.$table.' SET is_home=1 WHERE '.$table.'.cgn_web_publish_id='.$id; 
+		$db->query($sqlQuery01);
+
+		$this->presenter = 'redirect';
+		$t['url'] = cgn_adminurl('content','homepage','');
+	}
+
+	/**
+	 * Set the is_home data to '0'
+	 * in the cgn_web_publish table.
+	 */
+	function unsetHpEvent(&$req, &$t) {
+		$table = $req->cleanString('table');
+		$id    = $req->cleanInt($table.'_id');
+		if ($table != 'cgn_web_publish') {
+			return parent::setEvent($req,$t);
+		}
+
+		$db = Cgn_Db_Connector::getHandle();
+
+		$sqlQuery01 = 'UPDATE '.$table.' SET is_home=0 WHERE '.$table.'.cgn_web_publish_id='.$id; 
+		$db->query($sqlQuery01);
+
+		$this->presenter = 'redirect';
+		$t['url'] = cgn_adminurl('content','homepage','');
+	}
+
+
+
 }
 
-?>
