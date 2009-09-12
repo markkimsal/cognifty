71555ec3b00bcd481e2a3c6ef9a9a3d9a6b6b1d5
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2009-09-12 20:04:37 +0000

    Module Installer: ensure that phing is setup to work on Win32 as well as Unix.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@1928 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/mods/lib/Cgn_Install_Mgr.php
+++ src/cognifty/admin/mods/lib/Cgn_Install_Mgr.php
@@ -402,12 +402,14 @@ class Cgn_Phing_Command {
 		$this->isReady = TRUE;
 
 		/* set classpath */
-		if (!defined('PHP_CLASSPATH')) { define('PHP_CLASSPATH',  get_include_path().":./cognifty/lib"); }
+		if (!defined('PHP_CLASSPATH')) { define('PHP_CLASSPATH',  get_include_path().PATH_SEPARATOR."./cognifty/lib"); }
 		ini_set('include_path', PHP_CLASSPATH);
 
-		require_once 'phing/Phing.php';
+		require_once 'phing'.DIRECTORY_SEPARATOR.'Phing.php';
 
-		Phing::setProperty('host.fstype', 'UNIX');
+		//startup chooses the correct fstype
+		Phing::startup();
+//		Phing::setProperty('host.fstype', 'UNIX');
 		$buildFile = new PhingFile($this->commandFile);
 
 		$this->project = new Project();
