33c6af46699a4e5f4d7bf7ffa941a441e2743d16
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2010-03-06 17:17:29 +0000

    Utils: made script to find latest svn revision work with git-svn.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2011 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- globalsvnrev.sh
+++ globalsvnrev.sh
@@ -1,5 +1,22 @@
-REV="`svnversion -c -n . | awk -F: '{print $NF}' | sed -e '1,$s/[a-zA-Z\ ]*//g'`"; 
-sed -e "s/build\.number\=\(.*\)/build\.number\=$REV/" src/boot/core.ini > tmp.ini
+# Returns the svn version number of the current directory.
+# Works in an svn working copy, or in a git svn clone of an svn repo.
+# thanks pete goodliffe (http://goodliffe.blogspot.com/2009/08/code-how-to-spell-svnversion-in-git.html)
+
+alias git_svnversion="git svn find-rev `git log -1 --pretty=format:%H 2>/dev/null` 2>/dev/null"
+get_svnversion()
+{
+	SVNVERSION=`svnversion`
+	if [ "X$SVNVERSION" == "Xexported" -o "X$SVNVERSION" == "X" ]; then
+		git_svnversion
+	else
+		echo "`svnversion -c -n . | awk -F: '{print $NF}' | sed -e '1,$s/[a-zA-Z\ ]*//g'`"; 
+	fi
+}
+
+REV=$(get_svnversion)
+echo $REV;
+
+#REV=sed -e "s/build\.number\=\(.*\)/build\.number\=$REV/" src/boot/core.ini > tmp.ini
 if [ -f tmp.ini ]
 then
 	mv -f tmp.ini src/boot/core.ini
@@ -11,5 +28,14 @@ then
 	if [ -f tmp.ini ]
 	then
 		mv -f tmp.ini src/boot/local/core.ini
+	fi
+fi
+
+if [ -f src/boot/core.ini ]
+then
+	sed -e "s/build\.number\=\(.*\)/build\.number\=$REV/" src/boot/core.ini > tmp.ini
+	if [ -f tmp.ini ]
+	then
+		mv -f tmp.ini src/boot/core.ini
 	fi
 fi
