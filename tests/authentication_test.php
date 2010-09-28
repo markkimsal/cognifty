d4c152bce867c165bd64f7d70f7758c9dfaa9c0e
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2010-09-28 21:43:11 +0000

    Update tests for LDAP authenticator.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2049 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- tests/authentication_test.php
+++ tests/authentication_test.php
@@ -6,10 +6,12 @@ require_once(CGN_LIB_PATH.'/lib_cgn_db_connector.php');
 require_once(CGN_LIB_PATH.'/lib_cgn_db_mysql.php');
 
 Cgn::loadLibrary('lib_cgn_authc');
+Cgn::loadLibrary('lib_cgn_ldap');
 Cgn::loadLibrary('lib_cgn_error');
 
-Mock::generate('Cgn_Db_Connector');
 Mock::generate('Cgn_Db_Mysql');
+
+Mock::generate('Cgn_Ldap');
 
 class TestOfAuthentication extends UnitTestCase {
 
@@ -68,10 +70,59 @@ class TestOfAuthentication extends UnitTestCase {
 
 		$this->assertTrue(is_array($subj->attributes));
 		$this->assertEqual('foo@foo.localhost', $subj->attributes['email']);
+		/*
 		$this->assertEqual('EST', $subj->attributes['tzone']);
 		$this->assertEqual('zh_CN', $subj->attributes['locale']);
+		 */
 	}
 
+	function testLdapHandler() {
+		$uname = 'mkimsal';
+		$pass  = 'foobar';
+
+		$ldap = new Cgn_Authentication_Handler_Ldap();
+		$mockLdap = $this->setupLdap1();
+		$ldap->setLdapConn($mockLdap);
+
+		$authenticator = new Cgn_Authentication_Mgr(array('authDn'=>'ou=people,dc=example,dc=com', 'bindDn'=>'cn=%s,ou=people,dc=example,dc=com', 'dsn'=>"ldap://192.168.2.2"), $ldap);
+		$authenticator->connectUser = FALSE;
+		$loginRes = $authenticator->login($uname, $pass);
+		$subj = $authenticator->getSubject();
+
+		$this->assertTrue(is_array($subj->attributes));
+		$this->assertEqual('foo@foo.localhost', $subj->attributes['email']);
+
+		//no standard LDAP for locale and tzone settings
+		/*
+		$this->assertEqual('EST', $subj->attributes['tzone']);
+		$this->assertEqual('zh_CN', $subj->attributes['locale']);
+		 */
+	}
+
+	function testConnectUserToLocalDb() {
+		$uname = 'mkimsal';
+		$pass  = 'foobar';
+		$entryDn = 'cn=m kimsal,ou=people,dc=example,dc=com';
+
+		$ldap = new Cgn_Authentication_Handler_Ldap();
+		$mockLdap = $this->setupLdap1();
+		$ldap->setLdapConn($mockLdap);
+
+		$authenticator = new Cgn_Authentication_Mgr(array('authDn'=>'ou=people,dc=example,dc=com', 'bindDn'=>'cn=%s,ou=people,dc=example,dc=com', 'dsn'=>"ldap://192.168.2.2"), $ldap);
+		$authenticator->connectUser = TRUE;
+		$loginRes = $authenticator->login($uname, $pass);
+		$subj = $authenticator->getSubject();
+
+		$this->assertTrue(is_array($subj->attributes));
+		$this->assertEqual('foo@foo.localhost', $subj->attributes['email']);
+		$this->assertEqual($entryDn, $subj->attributes['dn']);
+
+		//no standard LDAP for locale and tzone settings
+		/*
+		$this->assertEqual('EST', $subj->attributes['tzone']);
+		$this->assertEqual('zh_CN', $subj->attributes['locale']);
+		 */
+	}
 
 	function setupLogin() {
 
@@ -83,10 +134,13 @@ class TestOfAuthentication extends UnitTestCase {
 		$dbRec =  array('number'=>1, 'cgn_user_id'=>1, 'email'=>'foo@foo.localhost', 'tzone'=>'EST', 'locale'=>'zh_CN');
 
 		$mysql = Cgn_Db_Connector::getHandle();
+
+		/*
 		$mysql->expectAtLeastOnce('query', array(
 			"SELECT * FROM cgn_user   where username = \"".$this->user->username."\"  and password = \"".$this->user->password."\"     ",
 			FALSE
 			));
+		 */
 		$mysql->setReturnValue('query',true);
 		$mysql->setReturnValue('nextRecord', false);
 		$mysql->setReturnValueAt(0, 'nextRecord', $dbRec);
@@ -99,4 +153,36 @@ class TestOfAuthentication extends UnitTestCase {
 		Cgn_DbWrapper::whenUsing('cgn_user', $mysql);
 	}
 
+	function setupLdap1() {
+		$ldap = new MockCgn_Ldap();
+
+		$ldapAttrs = 
+			array(
+		  	"entryUUID"=>
+				array (
+			  	"count"=>1,
+				 0=> "4c91fca6-01f8-102f-8ac1-43e1ab2be51f"
+  				),
+			0=>"entryUUID",
+
+			"mail"=>
+				array(
+			    "count"=>1,
+		      	0=> "foo@foo.localhost"
+				),
+			1=>"mail",
+			"entryDN"=>
+				array(
+					"count"=>1,
+					0=>"cn=m kimsal,ou=people,dc=example,dc=com"
+				),
+			2=>"entryDN"
+			);
+
+		$ldap->setReturnValue('bind',true);
+		$ldap->setReturnValue('search',true);
+		$ldap->setReturnValue('getAttributes', false);
+		$ldap->setReturnValueAt(0, 'getAttributes', $ldapAttrs);
+		return $ldap;
+	}
 }
