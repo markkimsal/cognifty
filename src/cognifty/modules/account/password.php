374e693b07510375a0ca4838b78fef6adecf10c7
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2010-10-10 17:59:34 +0000

    Fixed password not updating cached settings after change.  Updated look of password form.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2055 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/account/password.php
+++ src/cognifty/modules/account/password.php
@@ -24,31 +24,66 @@ class Cgn_Service_Account_Password extends Cgn_Service {
 	 */
 	function mainEvent(&$req, &$t) {
 
+		$t['form'] = $this->_loadPasswordForm();
 	}
 
 	/**
 	 * Process password change form.
 	 */
 	function changeEvent(&$req, &$t) {
-		$oldpwd  = $req->cleanString('password');
-		$newpwd  = $req->cleanString('newpassword');
-		$newpwd2 = $req->cleanString('newpassword2');
+		$oldpwd  = $req->cleanString('opwd');
+		$newpwd  = $req->cleanString('pwd1');
+		$newpwd2 = $req->cleanString('pwd2');
 		if ($req->getUser()->_hashPassword($oldpwd) != $req->getUser()->password) {
 		//	var_dump($req->getUser()->_hashPassword($oldpwd));exit();
-			$req->getUser()->addSessionMessage('Your password does not match.');
+			$req->getUser()->addSessionMessage('Your current password is not correct.');
+			$newTicket = new Cgn_SystemTicket($this->moduleName, $this->serviceName, 'main');
+			Cgn_SystemRunner::stackTicket($newTicket);
+
 			return false;
 		}
 		if ($newpwd != $newpwd2) {
-			$req->getUser()->addSessionMessage('Your password does not match.');
+			$req->getUser()->addSessionMessage('Your new password does not match.');
+			$newTicket = new Cgn_SystemTicket($this->moduleName, $this->serviceName, 'main');
+			Cgn_SystemRunner::stackTicket($newTicket);
+			return false;
+		}
+		if ( strlen(trim($newpwd)) < 5) {
+			$req->getUser()->addSessionMessage('Your new password must be at least six (6) characters long.');
+			$newTicket = new Cgn_SystemTicket($this->moduleName, $this->serviceName, 'main');
+			Cgn_SystemRunner::stackTicket($newTicket);
 			return false;
 		}
 
 		$user = $req->getUser();
 		$user->setPassword( $newpwd );
 		$user->save();
+		//current user has changed cached settings, rebind
+		$user->bindSession();
+
 		$req->getUser()->addSessionMessage('Your password has been updated.');
 		$this->presenter = 'redirect';
 		$t['url'] = cgn_appurl('account');
 	}
+
+
+	protected function _loadPasswordForm() {
+		Cgn::loadLibrary('Form::lib_cgn_form');
+		Cgn::loadLibrary('Html_widgets::lib_cgn_widget');
+
+		$f = new Cgn_Form('pwd_info');
+		$f->width = '40em';
+		$f->label = 'Change your password';
+
+		$f->layout = new Cgn_Form_Layout_Dl();
+
+		$f->action = cgn_sappurl('account', 'password', 'change');
+
+		$f->appendElement(new Cgn_Form_ElementPassword('opwd','Current Password'));
+		$f->appendElement(new Cgn_Form_ElementPassword('pwd1','New Password'));
+		$f->appendElement(new Cgn_Form_ElementPassword('pwd2','Confirm New Password'));
+
+//		$f->appendElement(new Cgn_Form_ElementHidden('id'),$values['id']);
+		return $f;
+	}
 }
-?>
--- src/cognifty/modules/account/templates/password_main.html.php
+++ src/cognifty/modules/account/templates/password_main.html.php
@@ -1,16 +1,2 @@
-<h2>Change your password</h2>
-<hr/>
-<form method="POST" action="<?=cgn_appurl('account','password','change');?>">
-<table cellpadding="0" cellspacing="0">
-<tr><td style="text-align:right">
-	<label for="oldpassword">Old Password:</label></td><td><input type="text" name="password" id="oldpassword"/>
-</td></tr>
-<tr><td style="text-align:right">
-<label for="newpassword">New Password:</label></td><td><input type="text" name="newpassword" id="newpassword"/>
-</td></tr>
-<tr><td style="text-align:right">
-<label for="newpassword2">Confirm Password:</label></td><td><input type="text" name="newpassword2" id="newpassword2"/>
-</td></tr>
-</table>
-<input type="submit" id="sbmt_button" name="sbmt_button" value="Change Password"/>
-</form>
+<?php
+echo $t['form']->toHtml();
