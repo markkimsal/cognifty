d1b2f7d93f8dca678be30e9a3e71ce3d33a0a23d
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2010-10-14 15:25:16 +0000

    New layout for account settings page.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2071 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/account/templates/main_main.html.php
+++ src/cognifty/modules/account/templates/main_main.html.php
@@ -1,26 +1,33 @@
 
+<h2>Profile Information</h2>
+
+<div style="border:1px solid black;padding:7px;">
+<div class="acct-sect-hdr" style="font-size:140%;">Contact Information</div>
+<span style="float:left;width:8.5em;">Display Name:</span> <span style="font-weight:bold;"><?=$u->getDisplayName();?></span>
+<br style="clear:both;"/>
+<span style="float:left;width:8.5em;clear:left;">E-mail:</span>      <span style="font-weight:bold;"><?=$u->email;?></span>
+<br style="clear:both;"/>
+<a href="<?=cgn_appurl('account','contact');?>">Change your contact information</a>
+</div>
+
+<br style="clear:both;"/>
+
+<div style="height:6em;border:1px solid black;padding:7px;overflow:hidden;">
+<img style="float:right;vertical-align:middle;margin-right:60%;" src="<?=cgn_appurl('account', 'img', '', array('r'=>rand()));?>"/>
+<span class="acct-sect-hdr" style="font-size:140%;">Profile Picture</span>
+<br/>
+<a href="<?=cgn_appurl('account', 'img', 'edit');?>">Change your picture</a>
+</div>
+
+<br style="clear:left;"/>
+
 <h2>Account Settings</h2>
-<hr/>
-
-<div class="acct-sect-hdr">Basic Information </div>
-Display Name: <?=$u->getDisplayName();?>
-<br/>
-E-mail:       <?=$u->email;?>
-<br/><a href="<?=cgn_appurl('account','contact');?>">Change your contact information</a>
-
-<br/>
-<br/>
-<div class="acct-sect-hdr">Picture</div>
-<img src="<?=cgn_appurl('account','img');?>"/>
-<br/>
-<a href="<?=cgn_appurl('account','img', 'edit');?>">Change your picture</a>
-
-<br/>
-<br/>
-<div class="acct-sect-hdr">Password</div>
+<div style="border:1px solid black;padding:7px;">
+<div class="acct-sect-hdr" style="font-size:140%;">Password</div>
 <a href="<?=cgn_appurl('account','password');?>">Change your password</a>
 
 <br/>
 <br/>
-<div class="acct-sect-hdr">Advanced Settings</div>
+<div class="acct-sect-hdr" style="font-size:140%;">Advanced Settings</div>
 <a href="<?=cgn_appurl('account','agent');?>">Setup your API agent</a>
+</div>
