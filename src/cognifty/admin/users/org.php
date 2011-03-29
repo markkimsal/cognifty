b7e69c81991308ce2835a1db6498c1c82567b5d7
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2011-03-29 18:53:07 +0000

    Fix display and saving of org records.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2091 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/admin/users/org.php
+++ src/cognifty/admin/users/org.php
@@ -38,7 +38,7 @@ class Cgn_Service_Users_Org extends Cgn_Service_AdminCrud {
 		$finder->_cols[] = 'COUNT(Torg.cgn_org_id) as member_count';
 		$finder->andWhere('cgn_account.cgn_user_id', 0);
 		$finder->hasOne('cgn_user_org_link', 'cgn_org_id', 'Torg');
-		$finder->groupBy('Torg.cgn_org_id');
+		$finder->groupBy('cgn_account.cgn_account_id');
 		return $finder->find();
 	}
 
@@ -61,25 +61,25 @@ class Cgn_Service_Users_Org extends Cgn_Service_AdminCrud {
 
 	/**
 	 * CRUD sub-classed methods
+	 * For "organizations" we only want to edit a few rows, not all
+	 * user account related fields.
 	 */
 	protected function _makeFormFields($f, $dataModel, $editMode=FALSE) {
 		$values = $dataModel->valuesAsArray();
 
-		if (!isset($values['display_name'])) {
-			$values['display_name'] = '';
+		$editable = array('display_name', 'org_name', 'org_id', 'ref_id', 'ref_no');
+		foreach ($editable as $_e) {
+			if (!isset($values[$_e])) {
+				$values[$_e] = '';
+			}
 		}
-		if (!isset($values['ref_id'])) {
-			$values['ref_id'] = '';
-		}
-		if (!isset($values['ref_no'])) {
-			$values['ref_no'] = '';
-		}
-
 		foreach ($values as $k=>$v) {
+			if (!in_array($k, $editable)) continue;
 			//don't add the primary key if we're in edit mode
 			if ($editMode == TRUE) {
 				if ($k == 'id' || $k == $dataModel->get('_table').'_id') continue;
 			}
+			$v = $this->formatValue($k, $v, $dataModel);
 			$widget = new Cgn_Form_ElementInput($k);
 			$widget->size = 55;
 			$f->appendElement($widget, $v);
@@ -90,10 +90,11 @@ class Cgn_Service_Users_Org extends Cgn_Service_AdminCrud {
 		}
 	}
 
+	/*
 	function saveEvent($req, &$t) {
 		$org = new Cgn_DataItem('cgn_account');
 		$org->set('org_name', $req->cleanString('display_name'));
-		$org->set('ref_id', $req->cleanString('code') );
+		$org->set('ref_id', $req->cleanString('ref_id') );
 		$org->set('ref_no', $req->cleanString('ref_no') );
 		$org->set('cgn_user_id', 0);
 		$org->save();
@@ -102,6 +103,7 @@ class Cgn_Service_Users_Org extends Cgn_Service_AdminCrud {
 		$t['url'] = cgn_adminurl(
 			'users','org');
 	}
+	 */
 
 
 	/**
