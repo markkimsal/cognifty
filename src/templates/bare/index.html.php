1312e279a7a422f979ac442d1ea0097bf69a2848
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2011-04-09 17:55:41 +0000

    Let the bare template include any dynamic javascript.  Also include the core system CSS.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2101 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/templates/bare/index.html.php
+++ src/templates/bare/index.html.php
@@ -2,7 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title><?php echo Cgn_Template::getPageTitle();?></title>
+<link href="<?php echo cgn_url();?>media/shared_css/system.css" rel="stylesheet" type="text/css" />
 <?= Cgn_Template::getSiteCss();?>
+<?= Cgn_Template::getSiteJs();?>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 </head>
 <body>
