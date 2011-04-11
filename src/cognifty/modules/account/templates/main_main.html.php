30812ba165b4b592a6f0ac957b31c40218853471
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2011-04-11 13:47:38 +0000

    Use HTTPS for account images if SSL is on so that you don't get mixed resources warnings in browsers.
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2106 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/modules/account/templates/main_main.html.php
+++ src/cognifty/modules/account/templates/main_main.html.php
@@ -13,7 +13,7 @@
 <br style="clear:both;"/>
 
 <div style="height:6em;border:1px solid black;padding:7px;overflow:hidden;">
-<img style="float:right;vertical-align:middle;margin-right:60%;" src="<?=cgn_appurl('account', 'img', '', array('r'=>rand()));?>"/>
+<img style="float:right;vertical-align:middle;margin-right:60%;" src="<?=cgn_sappurl('account', 'img', '', array('r'=>rand()));?>"/>
 <span class="acct-sect-hdr" style="font-size:140%;">Profile Picture</span>
 <br/>
 <a href="<?=cgn_appurl('account', 'img', 'edit');?>">Change your picture</a>
