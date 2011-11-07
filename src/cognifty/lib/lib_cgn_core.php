eccb141d8193c960bb93e7bce5850002acd301f5
markkimsal <markkimsal@3241a3d7-c3ea-42e9-b823-2be48aa1cd2b>
2011-11-07 13:15:13 +0000

    Fix CGI SAPI for non-broken CGI implementations (i.e. ones that don't use cgi.fix_pathinfo and don't supply ORIG_PATH_INFO)
    git-svn-id: https://niftyphp.svn.sourceforge.net/svnroot/niftyphp/cognifty/trunk@2165 3241a3d7-c3ea-42e9-b823-2be48aa1cd2b

--- src/cognifty/lib/lib_cgn_core.php
+++ src/cognifty/lib/lib_cgn_core.php
@@ -417,24 +417,30 @@ class Cgn_SystemRunner {
 			} else {
 				$parts = explode("/",substr($_SERVER['ORIG_PATH_INFO'],1));
 			}
-			$this->currentRequest->mse = urldecode($parts[0]);
-			array_shift($parts);
-			foreach($parts as $num=>$p) {
-				//only put url parts in the get and request
-				// if there's no equal sign
-				// otherwise you get duplicate entries "[0]=>foo=bar"
-				if (!strstr($p,'=')) {
-					$p = urldecode($p);
-					$params[$num] = $p;
-					$get[$num] = $p;
-				} else {
-					@list($k,$v) = explode("=",$p);
-					if ($v!='') {
-						$k = urldecode($k);
-						$v = urldecode($v);
-						$params[$k] = $v;
-						$get[$k] = $v;
-					}
+		} else {
+			if (substr($_SERVER['PATH_INFO'],-1) == '/' ) {
+				$parts = explode("/",substr($_SERVER['PATH_INFO'],1,-1));
+			} else {
+				$parts = explode("/",substr($_SERVER['PATH_INFO'],1));
+			}
+		}
+		$this->currentRequest->mse = urldecode($parts[0]);
+		array_shift($parts);
+		foreach($parts as $num=>$p) {
+			//only put url parts in the get and request
+			// if there's no equal sign
+			// otherwise you get duplicate entries "[0]=>foo=bar"
+			if (!strstr($p,'=')) {
+				$p = urldecode($p);
+				$params[$num] = $p;
+				$get[$num] = $p;
+			} else {
+				@list($k,$v) = explode("=",$p);
+				if ($v!='') {
+					$k = urldecode($k);
+					$v = urldecode($v);
+					$params[$k] = $v;
+					$get[$k] = $v;
 				}
 			}
 		}
